0e75d78dfb7f088519892e0dc28dfab7ead077ea
michael <michael@stickycode.net>
2010-09-24 03:59:24 +0000

    stale imports

--- net.stickycode.mockwire/sticky-mockwire/src/test/java/net/stickycode/mockwire/AbstractIsolationTest.java
+++ net.stickycode.mockwire/sticky-mockwire/src/test/java/net/stickycode/mockwire/AbstractIsolationTest.java
@@ -16,9 +16,6 @@ import javax.inject.Inject;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import net.stickycode.mockwire.junit4.MockwireRunner;
 
 import static org.fest.assertions.Assertions.assertThat;
 
