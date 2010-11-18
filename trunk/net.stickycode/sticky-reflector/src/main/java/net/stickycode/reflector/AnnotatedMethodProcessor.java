961cdb1a62b3f61ea990fcf1ef1e4a5a02394409
michael@stickycode.net <michael@stickycode.net@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2010-11-18 10:04:05 +0000

    Allow more than one annotation to act as the marker

--- trunk/net.stickycode/sticky-reflector/src/main/java/net/stickycode/reflector/AnnotatedMethodProcessor.java
+++ trunk/net.stickycode/sticky-reflector/src/main/java/net/stickycode/reflector/AnnotatedMethodProcessor.java
@@ -19,15 +19,19 @@ import java.util.List;
 public abstract class AnnotatedMethodProcessor
     implements MethodProcessor {
 
-  private Class<? extends Annotation> annotation;
+  private Class<? extends Annotation>[] annotation;
 
-  public AnnotatedMethodProcessor(Class<? extends Annotation> annotation) {
+  public AnnotatedMethodProcessor(Class<? extends Annotation>... annotation) {
     this.annotation = annotation;
   }
 
   @Override
   public boolean canProcess(Method method) {
-    return method.isAnnotationPresent(annotation);
+    for (Class<? extends Annotation> a : annotation)
+      if (method.isAnnotationPresent(a))
+        return true;
+
+    return false;
   }
 
   @Override
