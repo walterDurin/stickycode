c5546a8a5c57835f73ad41971d3f5de2e74e5311
michael@stickycode.net <michael@stickycode.net@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2010-11-18 10:32:19 +0000

    [Releasing sticky-deployer-tomcat6] copy for tag sticky-deployer-tomcat6-2.1

---
+++ tags/net.stickycode.deploy/sticky-deployer-tomcat6-2.1/src/main/java/net/stickycode/deploy/DeploymentConfiguration.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.deploy;
+
+import java.io.File;
+
+import net.stickycode.stereotype.Configured;
+
+public class DeploymentConfiguration {
+
+  @Configured
+  private File war;
+
+  @Configured
+  private int port = 8080;
+
+  @Configured
+  private String bindAddress = "localhost";
+
+  @Configured
+  private String contextPath = "";
+
+  @Configured
+  private File workingDirectory;
+
+  public DeploymentConfiguration() {
+    super();
+    this.workingDirectory = new File(System.getProperty("user.dir"));
+  }
+
+  public File getWar() {
+    return war;
+  }
+
+  public void setWar(File war) {
+    this.war = war;
+  }
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public String getBindAddress() {
+    return bindAddress;
+  }
+
+  public void setBindAddress(String bindAddress) {
+    this.bindAddress = bindAddress;
+  }
+
+  public String getContextPath() {
+    return contextPath;
+  }
+
+  public void setContextPath(String contextPath) {
+    this.contextPath = contextPath;
+  }
+
+  public File getWorkingDirectory() {
+    return workingDirectory;
+  }
+
+  public void setWorkingDirectory(File workingDirectory) {
+    this.workingDirectory = workingDirectory;
+  }
+}
