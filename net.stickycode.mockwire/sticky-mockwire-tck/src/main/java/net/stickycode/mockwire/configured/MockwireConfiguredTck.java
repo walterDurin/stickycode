183228e52d7dd1bfb0c18cfbf2b8eb077711cbd8
michael <michael@stickycode.net>
2010-12-22 01:43:06 +0000

    Turns out eclipse does not mind the trailing , but javac does.

--- net.stickycode.mockwire/sticky-mockwire-tck/src/main/java/net/stickycode/mockwire/configured/MockwireConfiguredTck.java
+++ net.stickycode.mockwire/sticky-mockwire-tck/src/main/java/net/stickycode/mockwire/configured/MockwireConfiguredTck.java
@@ -19,7 +19,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     ConfiguredTest.class,
-    MockwireConfigurationSourceTest.class,
+    MockwireConfigurationSourceTest.class
 })
 public class MockwireConfiguredTck {
 
