1cc23a6b47fa3467ef529eaeb3e6240046b54605
michael <michael@stickycode.net>
2011-01-03 03:22:11 +0000

    [Releasing sticky-mockwire] copy for tag sticky-mockwire-4.1

---
+++ sticky-mockwire-4.1/src/main/java/net/stickycode/mockwire/junit4/MockwireRunner.java
@@ -0,0 +1,69 @@
+package net.stickycode.mockwire.junit4;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+import net.stickycode.mockwire.Controlled;
+import net.stickycode.mockwire.MockwireContext;
+import net.stickycode.mockwire.UnderTest;
+
+/**
+ * A jUnit runner to make your test classes and code behave like it would when run live in a di context ala Mockwire.
+ *
+ * The default context used for Dependency Injection is a manifest defined by the test class itself. It will only contain
+ * {@link UnderTest code under test} and {@link Controlled controlled} classes in the actual test class or its super types.
+ *
+ * <pre>
+ * package net.stickycode.example;
+ *
+ *  &#064;RunWith(MockwireRunner.class)
+ *  public class ContainedTest {
+ *
+ *  &#064;UnderTest
+ *  SomeConcreteClass field;
+ *
+ *  &#064;Inject
+ *  SomeConcreteClass injectedInstanceOfBlessesClass;
+ *
+ *  &#064;Inject
+ *  IsolateTestContext context;
+ *
+ *  &#064;Test
+ *  public void testManifestHasCodeUnderTest() {
+ *    assertThat(context.hasRegisteredType(ConcreteClass.class)).isTrue();
+ *  }
+ * }
+ * </pre>
+ */
+public class MockwireRunner extends BlockJUnit4ClassRunner {
+
+  private final MockwireContext mockwire;
+
+  public MockwireRunner(Class<?> testClass) throws InitializationError {
+    super(testClass);
+    mockwire = new MockwireContext(testClass);
+  }
+
+// @Override
+//  protected Statement withAfterClasses(final Statement statement) {
+//    final Statement s = super.withAfterClasses(statement);
+//    return new Statement() {
+//
+//      @Override
+//      public void evaluate() throws Throwable {
+//        s.evaluate();
+//      }
+//
+//    };
+//  }
+
+  @Override
+  protected Object createTest() throws Exception {
+    mockwire.startup();
+    Object test = super.createTest();
+    mockwire.initialiseTestInstance(test);
+    mockwire.shutdown();
+    return test;
+  }
+
+}
