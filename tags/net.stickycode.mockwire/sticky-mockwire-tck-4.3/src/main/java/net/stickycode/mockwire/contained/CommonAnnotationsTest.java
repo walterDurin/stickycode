52e026e886b77c5d7ee83e171b5818a2c329ab85
michael@stickycode.net <michael@stickycode.net@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-04-12 02:17:35 +0000

    [Releasing sticky-mockwire-tck] copy for tag sticky-mockwire-tck-4.3

---
+++ tags/net.stickycode.mockwire/sticky-mockwire-tck-4.3/src/main/java/net/stickycode/mockwire/contained/CommonAnnotationsTest.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.mockwire.contained;
+
+import javax.annotation.PostConstruct;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import net.stickycode.mockwire.MockwireConfigured;
+import net.stickycode.mockwire.UnderTest;
+import net.stickycode.mockwire.junit4.MockwireRunner;
+import net.stickycode.stereotype.Configured;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+@RunWith(MockwireRunner.class)
+@MockwireConfigured("postConstructed.value=something")
+public class CommonAnnotationsTest {
+
+  @UnderTest
+  PostConstructed target;
+
+  @Before
+  public void before() {
+    assertThat(target).isNull();
+//    assertThat(target.initialised).isTrue();
+//    assertThat(target.destroyed).isFalse();
+//    assertThat(target.value).isNull();
+  }
+
+  @Test
+  public void postConstruct() {
+    assertThat(target).isNotNull();
+    assertThat(target.initialised).isTrue();
+    assertThat(target.destroyed).isFalse();
+    assertThat(target.value).isEqualTo("something");
+  }
+
+  @Test
+  public void postConstruct2() {
+    assertThat(target).isNotNull();
+    assertThat(target.initialised).isTrue();
+    assertThat(target.destroyed).isFalse();
+    assertThat(target.value).isEqualTo("something");
+  }
+
+  @After
+  public void after() {
+    assertThat(target.destroyed).isFalse();
+  }
+}
