da3c541be337c81de68c2410dcd8f892e5840719
michael@stickycode.net <michael@stickycode.net@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-04-19 20:06:53 +0000

    [Releasing sticky-configured-spring25] copy for tag sticky-configured-spring25-2.1

---
+++ tags/net.stickycode.configured/sticky-configured-spring25-2.1/src/main/java/net/stickycode/configured/spring25/ConfiguredBeanPostProcessor.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.configured.spring25;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
+
+import net.stickycode.configured.ConfigurationSystem;
+import net.stickycode.reflector.Reflector;
+import net.stickycode.stereotype.StickyComponent;
+
+/**
+ * Registers fields marked as configured for configuration
+ */
+@StickyComponent
+public class ConfiguredBeanPostProcessor
+    extends InstantiationAwareBeanPostProcessorAdapter {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  @Inject
+  private ConfigurationSystem configuration;
+
+  @Override
+  public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+    log.info("configuring {}", beanName);
+    new Reflector()
+        .forEachField(new ConfiguredFieldProcessor(configuration))
+        .process(bean);
+    return true;
+  }
+
+  public void setConfiguration(ConfigurationSystem configuration) {
+    this.configuration = configuration;
+  }
+
+}
