20556d0d798367950502554e15f51ca8cc1a8327
michael <michael@stickycode.net>
2011-04-20 11:07:34 +0000

    Add a configured collection test

--- net.stickycode.mockwire/sticky-mockwire-tck/src/main/java/net/stickycode/mockwire/configured/MockwireConfiguredTck.java
+++ net.stickycode.mockwire/sticky-mockwire-tck/src/main/java/net/stickycode/mockwire/configured/MockwireConfiguredTck.java
@@ -19,6 +19,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     ConfiguredTest.class,
+    ConfiguredCollectionTest.class,
     MockwireConfigurationSourceTest.class
 })
 public class MockwireConfiguredTck {
