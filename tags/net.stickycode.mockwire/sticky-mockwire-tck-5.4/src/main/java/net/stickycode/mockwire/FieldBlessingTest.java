9a9692f30f347dd0b505a70cf2af1b785bbd1f89
mockwire@gmail.com <mockwire@gmail.com@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-06-02 10:58:18 +0000

    [Releasing sticky-mockwire-tck] copy for tag sticky-mockwire-tck-5.4

---
+++ tags/net.stickycode.mockwire/sticky-mockwire-tck-5.4/src/main/java/net/stickycode/mockwire/FieldBlessingTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.mockwire;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FieldBlessingTest {
+
+  @UnderTest
+  private Autowirable autowirable;
+
+	@Inject
+	private Autowirable injected;
+
+	@Inject
+	IsolatedTestManifest context;
+
+	@Test
+	public void underTest() {
+	  Mockwire.isolate(this);
+	  assertThat(context.hasRegisteredType(Autowirable.class)).isTrue();
+	  assertThat(injected).isNotNull();
+	  assertThat(autowirable).isNotNull();
+	}
+}
