04fa247bc160c201f4479c61cf3a65ed67051caf
mockwire <mockwire@gmail.com>
2011-07-14 02:57:52 +0000

    [Releasing sticky-mockwire-guice3] copy for tag sticky-mockwire-guice3-0.6

---
+++ sticky-mockwire-guice3-0.6/src/test/java/net/stickycode/mockwire/guice3/ConfiguredTck.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.mockwire.guice3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import net.stickycode.mockwire.configured.MockwireConfiguredTck;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MockwireConfiguredTck.class })
+public class ConfiguredTck {
+
+  /**
+   * This is an anchor for Infinitest to rerun this suite if its changes
+   */
+  GuiceIsolatedTestManifest infinitestRememberThisSuite;
+
+}
