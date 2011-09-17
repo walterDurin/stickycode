64e1b8dfc81aa1e10f549b711951b00fa934f8bb
mockwire <mockwire@gmail.com>
2011-09-17 12:13:39 +0000

    Turn the spring30 impl into spring25

--- net.stickycode.scheduled/sticky-scheduled-spring25/src/test/java/net/stickycode/scheduled/spring25/ScheduleBeanPostProcessorTest.java
+++ net.stickycode.scheduled/sticky-scheduled-spring25/src/test/java/net/stickycode/scheduled/spring25/ScheduleBeanPostProcessorTest.java
@@ -10,13 +10,14 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package net.stickycode.scheduled.spring30;
+package net.stickycode.scheduled.spring25;
 
 import static org.mockito.Mockito.verify;
 import net.stickycode.configured.ConfigurationRepository;
 import net.stickycode.configured.ConfiguredConfiguration;
 import net.stickycode.scheduled.ScheduledMethodInvoker;
 import net.stickycode.scheduled.ScheduledRunnableRepository;
+import net.stickycode.scheduled.spring25.ScheduledBeanPostProcessor;
 import net.stickycode.stereotype.Scheduled;
 
 import org.junit.Test;
