c9f695351fb78e10cf7b1db3f4265204106ebde2
mockwire@gmail.com <mockwire@gmail.com@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-09-21 11:27:11 +0000

    [Releasing sticky-scheduled-guice3] copy for tag sticky-scheduled-guice3-0.3

---
+++ tags/net.stickycode.scheduled/sticky-scheduled-guice3-0.3/src/main/java/net/stickycode/scheduled/guice3/ScheduledInjector.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.scheduled.guice3;
+
+import net.stickycode.configured.ConfigurationRepository;
+import net.stickycode.configured.ConfiguredConfiguration;
+import net.stickycode.reflector.Reflector;
+import net.stickycode.scheduled.ScheduledMethodProcessor;
+import net.stickycode.scheduled.ScheduledRunnableRepository;
+import net.stickycode.stereotype.StickyComponent;
+
+import com.google.inject.Inject;
+import com.google.inject.MembersInjector;
+
+@StickyComponent
+public class ScheduledInjector
+    implements MembersInjector<Object> {
+
+  @Inject
+  private ConfigurationRepository configurationRepository;
+
+  @Inject
+  private ScheduledRunnableRepository schedulingSystem;
+
+  @Override
+  public void injectMembers(Object instance) {
+    ConfiguredConfiguration configuration = new ConfiguredConfiguration(instance);
+    new Reflector()
+        .forEachMethod(new ScheduledMethodProcessor(schedulingSystem, configuration))
+        .process(instance);
+    configurationRepository.register(configuration);
+  }
+
+}
