e052ada024c7d507298a3177f3f32a862654a453
mockwire <mockwire@gmail.com>
2011-09-27 03:45:22 +0000

    [Releasing sticky-scheduled-guice3] copy for tag sticky-scheduled-guice3-0.5

---
+++ sticky-scheduled-guice3-0.5/src/main/java/net/stickycode/scheduled/guice3/ScheduledTypeListener.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.scheduled.guice3;
+
+import java.lang.reflect.Method;
+
+import javax.inject.Inject;
+
+import net.stickycode.stereotype.Scheduled;
+import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.stereotype.StickyFramework;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
+
+@StickyComponent
+@StickyFramework
+public class ScheduledTypeListener
+    implements TypeListener {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  @Inject
+  private ScheduledInjector membersInjector;
+
+  @Override
+  public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
+    if (typeIsScheduled(type)) {
+      if (membersInjector == null)
+        throw new AssertionError("on hearing " + type.getRawType().getName() + " found that " + getClass().getSimpleName() + " was not injected with a " + ScheduledInjector.class.getSimpleName());
+
+      encounter.register(membersInjector);
+      log.info("encountering {} registering injector {}", type, membersInjector);
+    }
+  }
+
+  private <I> boolean typeIsScheduled(TypeLiteral<I> type) {
+    for (Method method : type.getRawType().getDeclaredMethods())
+      if (method.isAnnotationPresent(Scheduled.class))
+        return true;
+
+    return false;
+  }
+}
