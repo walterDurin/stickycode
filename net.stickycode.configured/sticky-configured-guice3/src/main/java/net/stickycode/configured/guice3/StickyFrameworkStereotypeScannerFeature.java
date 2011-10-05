33a54aa4911fab2aa95f6966e15232e5465c1c8e
mockwire <mockwire@gmail.com>
2011-10-05 03:54:49 +0000

    Consolidate the implementations of the stereotype feature scanners, was a bit copy and pasted

--- net.stickycode.configured/sticky-configured-guice3/src/main/java/net/stickycode/configured/guice3/StickyFrameworkStereotypeScannerFeature.java
+++ net.stickycode.configured/sticky-configured-guice3/src/main/java/net/stickycode/configured/guice3/StickyFrameworkStereotypeScannerFeature.java
@@ -13,21 +13,11 @@
 package net.stickycode.configured.guice3;
 
 import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import net.stickycode.stereotype.StickyFramework;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.inject.MembersInjector;
-import com.google.inject.Scopes;
 import com.google.inject.Singleton;
-import com.google.inject.matcher.Matchers;
-import com.google.inject.spi.TypeListener;
 
 import de.devsurf.injection.guice.install.InstallationContext.BindingStage;
 
@@ -35,8 +25,6 @@ import de.devsurf.injection.guice.install.InstallationContext.BindingStage;
 public class StickyFrameworkStereotypeScannerFeature
     extends StickyStereotypeScannerFeature {
   
-  private Logger log = LoggerFactory.getLogger(StickyFrameworkStereotypeScannerFeature.class);
-
   @Override
   public BindingStage accept(Class<Object> annotatedClass, Map<String, Annotation> annotations) {
     if (!annotatedClass.isAnnotationPresent(StickyFramework.class))
@@ -52,52 +40,5 @@ public class StickyFrameworkStereotypeScannerFeature
 
     return BindingStage.IGNORE;
   }
-
-  @Override
-  @SuppressWarnings("unchecked")
-  public void process(Class<Object> annotatedClass, Map<String, Annotation> annotations) {
-    log.info("process {} with {}", annotatedClass, annotations);
-    
-    Class<Object>[] interfaces = (Class<Object>[]) annotatedClass.getInterfaces();
-
-    if (interfaces.length == 0) {
-      List<Class<?>> interfaceCollection = new ArrayList<Class<?>>();
-      Class<? super Object> parent = annotatedClass.getSuperclass();
-      while (parent != null && !parent.equals(Object.class)) {
-        Collections.addAll(interfaceCollection, parent.getInterfaces());
-        parent = parent.getSuperclass();
-      }
-      interfaces = interfaceCollection.toArray(new Class[interfaceCollection.size()]);
-    }
-    if (interfaces.length == 0) {
-      bind(annotatedClass, null, Scopes.SINGLETON);
-    }
-    for (Class<Object> interf : interfaces) {
-      if (interf.isAssignableFrom(MembersInjector.class))
-        bind(annotatedClass, null, Scopes.SINGLETON);
-      else
-        if (interf.isAssignableFrom(TypeListener.class))
-          bindListener(annotatedClass);
-        else
-          bind(annotatedClass, interf, (Annotation) null, Scopes.SINGLETON);
-    }
-  }
-
-  private void bindListener(Class<Object> annotatedClass) {
-    TypeListener typeListener = typeListener(annotatedClass);
-    _binder.requestInjection(typeListener);
-    _binder.bindListener(Matchers.any(), typeListener);
-  }
-
-  private TypeListener typeListener(Class<Object> annotatedClass) {
-    try {
-      return (TypeListener) annotatedClass.newInstance();
-    }
-    catch (InstantiationException e) {
-      throw new RuntimeException(e);
-    }
-    catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-  }
+ 
 }
