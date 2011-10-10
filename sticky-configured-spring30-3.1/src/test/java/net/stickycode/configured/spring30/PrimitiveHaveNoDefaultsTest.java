420a82686012d936f284e7a42ba2f0b6744115b7
mockwire <mockwire@gmail.com>
2011-10-10 09:10:26 +0000

    [Releasing sticky-configured-spring30] copy for tag sticky-configured-spring30-3.1

---
+++ sticky-configured-spring30-3.1/src/test/java/net/stickycode/configured/spring30/PrimitiveHaveNoDefaultsTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.configured.spring30;
+
+import java.beans.Introspector;
+
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.support.GenericApplicationContext;
+
+import net.stickycode.coercion.Coercions;
+import net.stickycode.coercion.PatternCoercion;
+import net.stickycode.configured.AbstractPrimitiveConfiguratedTest;
+import net.stickycode.configured.ConfigurationSource;
+import net.stickycode.configured.ConfigurationSystem;
+import net.stickycode.configured.InlineConfigurationRepository;
+import net.stickycode.configured.SimpleNameDotFieldConfigurationKeyBuilder;
+
+public class PrimitiveHaveNoDefaultsTest
+    extends AbstractPrimitiveConfiguratedTest {
+
+  protected void configure(Object instance, ConfigurationSource configurationSource) {
+    GenericApplicationContext c = new GenericApplicationContext();
+    c.getBeanFactory().registerSingleton(name(ConfigurationSource.class), configurationSource);
+
+    registerType(c, PatternCoercion.class);
+    registerType(c, Coercions.class);
+    registerType(c, InlineConfigurationRepository.class);
+    registerType(c, SimpleNameDotFieldConfigurationKeyBuilder.class);
+    registerType(c, ConfigurationSystem.class);
+    registerType(c, ConfiguredBeanPostProcessor.class);
+    registerType(c, AutowiredAnnotationBeanPostProcessor.class);
+
+    c.refresh();
+
+    c.getAutowireCapableBeanFactory().autowireBean(instance);
+    c.getAutowireCapableBeanFactory().autowireBean(this);
+  }
+
+  public void registerType(GenericApplicationContext c, Class<?> type) {
+    GenericBeanDefinition bd = new GenericBeanDefinition();
+    bd.setBeanClass(type);
+    bd.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_BY_TYPE);
+    c.getDefaultListableBeanFactory().registerBeanDefinition(name(type), bd);
+  }
+
+  private String name(Class<?> type) {
+    return Introspector.decapitalize(type.getSimpleName());
+  }
+
+}
