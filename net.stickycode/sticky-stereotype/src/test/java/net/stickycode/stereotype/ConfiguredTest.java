36f6ef7ce876af3c85250f3b41a8334124ed8631
mockwire <mockwire@gmail.com>
2011-10-26 18:57:42 +0000

    Moving Content configuration to a separate package as its stereotypes and interfaces

--- net.stickycode/sticky-stereotype/src/test/java/net/stickycode/stereotype/ConfiguredTest.java
+++ net.stickycode/sticky-stereotype/src/test/java/net/stickycode/stereotype/ConfiguredTest.java
@@ -16,16 +16,6 @@ public class ConfiguredTest {
     @Configured("Set to true if the light should be on")
     Boolean lightOn;
 
-    @Schedule
-    public void runMe() {
-
-    }
-
-    @Schedule("Print the configured value to Standard.out")
-    public void runMe2() {
-      System.out.println(configured);
-    }
-
   }
 
   @Test
