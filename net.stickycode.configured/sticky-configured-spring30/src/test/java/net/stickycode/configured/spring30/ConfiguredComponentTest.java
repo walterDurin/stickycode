434a1274421bd85b9e8a306064514322999aa7d3
mockwire <mockwire@gmail.com>
2011-10-27 04:18:04 +0000

    Extract the configuration lifecycle from the Configured implementation of it, this allows for other listerners to hook into the lifecycle as well e.g. for content

--- net.stickycode.configured/sticky-configured-spring30/src/test/java/net/stickycode/configured/spring30/ConfiguredComponentTest.java
+++ net.stickycode.configured/sticky-configured-spring30/src/test/java/net/stickycode/configured/spring30/ConfiguredComponentTest.java
@@ -22,6 +22,7 @@ import net.stickycode.configured.AbstractConfiguredComponentTest;
 import net.stickycode.configured.ConfigurationManifest;
 import net.stickycode.configured.ConfigurationSource;
 import net.stickycode.configured.ConfigurationSystem;
+import net.stickycode.configured.ConfiguredConfigurationListener;
 import net.stickycode.configured.InlineConfigurationRepository;
 import net.stickycode.configured.SimpleNameDotFieldConfigurationKeyBuilder;
 
@@ -50,6 +51,7 @@ public class ConfiguredComponentTest
     registerType(c, ConfigurationManifest.class);
     registerType(c, SimpleNameDotFieldConfigurationKeyBuilder.class);
     registerType(c, ConfigurationSystem.class);
+    registerType(c, ConfiguredConfigurationListener.class);
     registerType(c, ConfiguredBeanPostProcessor.class);
     registerType(c, AutowiredAnnotationBeanPostProcessor.class);
 
