431362f476a4e11cf02b4f95116a0abc01499a26
mockwire@gmail.com <mockwire@gmail.com@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-11-10 09:34:43 +0000

    [Releasing sticky-reflector] copy for tag sticky-reflector-1.7

---
+++ tags/net.stickycode/sticky-reflector-1.7/src/main/java/net/stickycode/reflector/AnnotatedFieldSettingProcessor.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.reflector;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+public class AnnotatedFieldSettingProcessor
+    extends AnnotatedFieldProcessor {
+
+  private final ValueSource values;
+
+  public AnnotatedFieldSettingProcessor(
+      ValueSource values,
+      Class<? extends Annotation>... annotation) {
+    super(annotation);
+    this.values = values;
+  }
+
+  @Override
+  public void processField(Object target, Field field) {
+    field.setAccessible(true);
+    try {
+      field.set(target, values.get(field.getType()));
+    }
+    catch (IllegalArgumentException e) {
+      throw new RuntimeException(e);
+    }
+    catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
