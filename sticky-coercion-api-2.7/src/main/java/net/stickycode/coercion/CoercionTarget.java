04c45a6d996c7e671c31abf9a2da01e6dea73568
mockwire <mockwire@gmail.com>
2011-11-22 08:24:19 +0000

    [Releasing sticky-coercion-api] copy for tag sticky-coercion-api-2.7

---
+++ sticky-coercion-api-2.7/src/main/java/net/stickycode/coercion/CoercionTarget.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.coercion;
+
+import java.lang.annotation.Annotation;
+
+
+/**
+ * Contract for something that can be the target of a coercion.
+ *
+ */
+public interface CoercionTarget {
+
+  Class<?> getType();
+
+  boolean isArray();
+
+  Class<?> getComponentType();
+
+  CoercionTarget getComponentCoercionType();
+
+  CoercionTarget[] getComponentCoercionTypes();
+  
+  /**
+   * Return true if this coercion target is annotated with the given metadata
+   */
+  boolean hasAnnotation(Class<? extends Annotation> metadata);
+
+}
