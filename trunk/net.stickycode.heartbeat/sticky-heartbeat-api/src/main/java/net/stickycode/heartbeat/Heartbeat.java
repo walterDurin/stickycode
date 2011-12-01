04fd863509981d371a09c1ebebf6db80e307d7c1
mockwire@gmail.com <mockwire@gmail.com@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-12-01 03:45:56 +0000

    The Heartbeat and collections of them and a scheduled method invoked factory extending the scheduled framework

---
+++ trunk/net.stickycode.heartbeat/sticky-heartbeat-api/src/main/java/net/stickycode/heartbeat/Heartbeat.java
@@ -0,0 +1,8 @@
+package net.stickycode.heartbeat;
+
+
+public interface Heartbeat {
+
+  boolean isAlive();
+
+}
---
+++ trunk/net.stickycode.heartbeat/sticky-heartbeat/src/main/java/net/stickycode/heartbeat/HeartbeatMethodsMustNotHaveAnyParametersException.java
@@ -0,0 +1,17 @@
+package net.stickycode.heartbeat;
+
+import java.lang.reflect.Method;
+
+import net.stickycode.exception.PermanentException;
+
+@SuppressWarnings("serial")
+public class HeartbeatMethodsMustNotHaveAnyParametersException
+    extends PermanentException {
+
+  public HeartbeatMethodsMustNotHaveAnyParametersException(Method method) {
+    super("Heartbeat methods should not have parameters yet I found {} on {}.{}",
+        method.getParameterTypes(), method.getDeclaringClass().getName(), method.getName());
+
+  }
+
+}
