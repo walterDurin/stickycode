c796fdfa342ceb6d95813778fc2bd1310160d6ab
mockwire@gmail.com <mockwire@gmail.com@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2011-12-08 20:14:13 +0000

    [Releasing sticky-scheduled-api] copy for tag sticky-scheduled-api-2.1

---
+++ tags/net.stickycode.scheduled/sticky-scheduled-api-2.1/src/main/java/net/stickycode/scheduled/ScheduledMethodInvokerFactory.java
@@ -0,0 +1,25 @@
+package net.stickycode.scheduled;
+
+import java.lang.reflect.Method;
+
+import net.stickycode.stereotype.StickyFramework;
+
+/**
+ * This abstraction allows for componentised extension of scheduling for methods.
+ * 
+ * Each factory defines if a given method can be invoked by the {@link ScheduledRunnable}'s that this factory is responsible for.
+ */
+@StickyFramework
+public interface ScheduledMethodInvokerFactory {
+
+  /**
+   * Return true if the {@link ScheduledRunnable}'s of this factory invoke the given method
+   */
+  boolean canInvoke(Method method);
+
+  /**
+   * Create a scheduled runnable that will be used to invoke {@code method} on target with the given {@link Schedule schedule}
+   */
+  ScheduledRunnable create(Object target, Method method, Schedule schedule);
+
+}
