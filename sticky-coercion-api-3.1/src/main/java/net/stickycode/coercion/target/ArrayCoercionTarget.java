3e4898e33061c3e1da93b5ab016f43020df411f3
mockwire <mockwire@gmail.com>
2011-12-30 11:13:37 +0000

    [Releasing sticky-coercion-api] copy for tag sticky-coercion-api-3.1

---
+++ sticky-coercion-api-3.1/src/main/java/net/stickycode/coercion/target/ArrayCoercionTarget.java
@@ -0,0 +1,78 @@
+package net.stickycode.coercion.target;
+
+import net.stickycode.coercion.CoercionTarget;
+
+public class ArrayCoercionTarget
+    implements CoercionTarget {
+
+  private final Class<?> type;
+
+  public ArrayCoercionTarget(Class<?> type) {
+    super();
+    if (!type.isArray())
+      throw new ArrayCoercionTargetsMustTargetArraysException(type);
+
+    this.type = type;
+  }
+
+  @Override
+  public Class<?> getType() {
+    return type;
+  }
+
+  @Override
+  public boolean hasComponents() {
+    return true;
+  }
+
+  @Override
+  public CoercionTarget[] getComponentCoercionTypes() {
+    return new CoercionTarget[] { CoercionTargets.find(type.getComponentType()) };
+  }
+
+  @Override
+  public boolean isArray() {
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * type.getName().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+
+    if (obj == null)
+      return false;
+
+    if (getClass() != obj.getClass())
+      return false;
+
+    ArrayCoercionTarget other = (ArrayCoercionTarget) obj;
+    if (type == null) {
+      if (other.type != null)
+        return false;
+    }
+
+    return type == other.type;
+  }
+
+  @Override
+  public boolean isPrimitive() {
+    return false;
+  }
+
+  @Override
+  public Class<?> boxedType() {
+    throw new UnsupportedOperationException("No boxed type here, move along please");
+  }
+  
+  @Override
+  public String toString() {
+    return "COercionTarget{" + type.getSimpleName() + "}";
+  }
+
+}
