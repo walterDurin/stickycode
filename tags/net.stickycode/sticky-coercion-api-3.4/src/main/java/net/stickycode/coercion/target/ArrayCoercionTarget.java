16532285050d9f4f3994b2e4b92b81d8991b001b
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-01-19 09:23:02 +0000

    [Releasing sticky-coercion-api] copy for tag sticky-coercion-api-3.4

---
+++ tags/net.stickycode/sticky-coercion-api-3.4/src/main/java/net/stickycode/coercion/target/ArrayCoercionTarget.java
@@ -0,0 +1,97 @@
+package net.stickycode.coercion.target;
+
+import java.lang.reflect.AnnotatedElement;
+
+import net.stickycode.coercion.CoercionTarget;
+
+public class ArrayCoercionTarget
+    implements CoercionTarget {
+
+  private final Class<?> type;
+  private final Class<?> owner;
+
+  public ArrayCoercionTarget(Class<?> type, Class<?> owner) {
+    super();
+    if (!type.isArray())
+      throw new ArrayCoercionTargetsMustTargetArraysException(type);
+
+    this.type = type;
+    this.owner = owner;
+  }
+
+  @Override
+  public Class<?> getType() {
+    return type;
+  }
+
+  @Override
+  public boolean hasComponents() {
+    return true;
+  }
+
+  @Override
+  public CoercionTarget[] getComponentCoercionTypes() {
+    return new CoercionTarget[] { CoercionTargets.find(type.getComponentType()) };
+  }
+
+  @Override
+  public boolean isArray() {
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 31 * type.getName().hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+
+    if (obj == null)
+      return false;
+
+    if (getClass() != obj.getClass())
+      return false;
+
+    ArrayCoercionTarget other = (ArrayCoercionTarget) obj;
+    if (type == null) {
+      if (other.type != null)
+        return false;
+    }
+
+    return type == other.type;
+  }
+
+  @Override
+  public boolean isPrimitive() {
+    return false;
+  }
+
+  @Override
+  public Class<?> boxedType() {
+    throw new UnsupportedOperationException("No boxed type here, move along please");
+  }
+  
+  @Override
+  public String toString() {
+    return "COercionTarget{" + type.getSimpleName() + "}";
+  }
+
+  @Override
+  public boolean canBeAnnotated() {
+    return false;
+  }
+
+  @Override
+  public AnnotatedElement getAnnotatedElement() {
+    throw new UnsupportedOperationException("Arrays cannot be annotated directly");
+  }
+
+  @Override
+  public Class<?> getOwner() {
+    return owner;
+  }
+
+}
