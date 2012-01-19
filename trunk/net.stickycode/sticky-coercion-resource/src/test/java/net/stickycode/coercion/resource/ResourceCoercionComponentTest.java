5168eb4eac32602c9275e7439bed53537371fdb3
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-01-19 13:27:50 +0000

    Make Resources configurable too

---
+++ trunk/net.stickycode/sticky-coercion-resource/src/test/java/net/stickycode/coercion/resource/ResourceCoercionComponentTest.java
@@ -0,0 +1,61 @@
+package net.stickycode.coercion.resource;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import net.stickycode.coercion.CoercionTarget;
+import net.stickycode.coercion.target.CoercionTargets;
+import net.stickycode.mockwire.MockwireConfigured;
+import net.stickycode.mockwire.MockwireContainment;
+import net.stickycode.mockwire.junit4.MockwireRunner;
+import net.stickycode.reflector.Fields;
+import net.stickycode.resource.Resource;
+
+import org.fest.assertions.MapAssert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(MockwireRunner.class)
+@MockwireContainment
+@MockwireConfigured({})
+public class ResourceCoercionComponentTest {
+
+  @XmlRootElement
+  public static class Bean {
+
+      @XmlAttribute
+      private String value;
+  }
+
+  private Resource<String> stringResource;
+
+  private Resource<Properties> propertiesResource;
+
+  private Resource<Bean> bean;
+
+  @Inject
+  private ResourceCoercion coercion;
+
+  @Test
+  public void jaxbResource() {
+    CoercionTarget target = target("bean");
+    Bean bean = (Bean)coercion.coerce(target, "::classpath://bean.xml").get();
+    assertThat(bean.value).isEqualTo("loaded");
+  }
+  
+  @Test
+  public void propertiesResource() {
+    CoercionTarget target = target("propertiesResource");
+    Properties bean = (Properties)coercion.coerce(target, "some.properties").get();
+    assertThat(bean).includes(MapAssert.entry("a", "b"));
+  }
+
+  private CoercionTarget target(String fieldName) {
+    return CoercionTargets.find(Fields.find(getClass(), fieldName));
+  }
+}
