a318ecf34f1402e04d1205a28788d8a05f6c872f
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-02-10 04:00:31 +0000

    [Releasing sticky-configured-content] copy for tag sticky-configured-content-2.1

---
+++ tags/net.stickycode.configured/sticky-configured-content-2.1/src/main/java/net/stickycode/configured/content/FixedContent.java
@@ -0,0 +1,19 @@
+package net.stickycode.configured.content;
+
+import net.stickycode.stereotype.ui.Content;
+
+public class FixedContent
+    implements Content {
+
+  private final String value;
+
+  public FixedContent(String value) {
+    this.value = value;
+  }
+
+  @Override
+  public String get() {
+    return value;
+  }
+
+}
