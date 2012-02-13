bb718a507894d2f6484fa4d37470f12f4692c3ae
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-02-13 03:57:25 +0000

    [Releasing sticky-configured-tck] copy for tag sticky-configured-tck-4.2

---
+++ tags/net.stickycode.configured/sticky-configured-tck-4.2/src/main/java/net/stickycode/configured/finder/SingletonBean.java
@@ -0,0 +1,8 @@
+package net.stickycode.configured.finder;
+
+import net.stickycode.stereotype.StickyComponent;
+
+@StickyComponent
+public class SingletonBean {
+
+}
---
+++ tags/net.stickycode.configured/sticky-configured-tck-4.2/src/main/java/net/stickycode/configured/finder/TooManyOne.java
@@ -0,0 +1,9 @@
+package net.stickycode.configured.finder;
+
+import net.stickycode.stereotype.StickyComponent;
+
+@StickyComponent
+public class TooManyOne
+    implements TooMany {
+
+}
