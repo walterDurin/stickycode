b2b070674feb50f3bd024fe8032995cf7509bedb
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-02-21 04:14:43 +0000

    Extracted the common api/ws class from spring and guice examples

--- co.nfigured.examples/configured-example-guice3-ws-deploy/src/test/java/co/nfigured/examples/ws/helloworld/v1/EchoingHelloWorldIntegrationTest.java
+++ co.nfigured.examples/configured-example-guice3-ws-deploy/src/test/java/co/nfigured/examples/ws/helloworld/v1/EchoingHelloWorldIntegrationTest.java
@@ -20,7 +20,7 @@ import net.stickycode.stereotype.Configured;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import co.nfigured.examples.ws.helloworld.v1.HelloWorld;
+import co.nfigured.example.helloworld.v1.HelloWorld;
 
 @RunWith(MockwireRunner.class)
 @MockwireConfigured
