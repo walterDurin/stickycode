a08eb815300953b66a921edb0441544261c13e91
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-02-27 04:05:02 +0000

    The test that proves issue-9 is fixed... Array coercions do not honour all the default coercions

--- trunk/net.stickycode/sticky-coercion-3/src/test/java/net/stickycode/coercion/ArrayCoercionTest.java
+++ trunk/net.stickycode/sticky-coercion-3/src/test/java/net/stickycode/coercion/ArrayCoercionTest.java
@@ -12,14 +12,40 @@
  */
 package net.stickycode.coercion;
 
-import java.util.Arrays;
+import static org.fest.assertions.Assertions.assertThat;
 
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
-
 public class ArrayCoercionTest {
+  private static class Blah {
+    private final String value;
 
+    public Blah(String value) {
+      super();
+      this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+      return value.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      return value.equals(((Blah)obj).value);
+    }
+    
+  }
+  
+  @Test
+  public void stringConstructor() {
+    ArrayCoercion coercion = coercion();
+    CoercionTarget coercionTarget = coercionTarget(Blah[].class);
+    assertThat(coercion.isApplicableTo(coercionTarget)).isTrue();
+    assertThat((Blah[]) coercion.coerce(coercionTarget, "a")).containsOnly(new Blah("a"));
+    assertThat((Blah[]) coercion.coerce(coercionTarget, "a,b,c")).containsOnly(new Blah("a"), new Blah("b"), new Blah( "c"));
+    assertThat((Blah[]) coercion.coerce(coercionTarget, "abc,bde,ceg")).containsOnly(new Blah("abc"), new Blah("bde"), new Blah( "ceg"));
+  }
   @Test
   public void string() {
     ArrayCoercion coercion = coercion();
