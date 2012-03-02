c6ae2f9d426bdbef5b55d3122919330fa6dd8a6f
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-02 09:56:05 +0000

    [Releasing sticky-mockwire] copy for tag sticky-mockwire-7.2

---
+++ tags/net.stickycode.mockwire/sticky-mockwire-7.2/src/main/java/net/stickycode/mockwire/ControlledAnnotatedFieldProcessor.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.mockwire;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+import net.stickycode.reflector.AnnotatedFieldProcessor;
+
+class ControlledAnnotatedFieldProcessor
+    extends AnnotatedFieldProcessor {
+
+  private final IsolatedTestManifest manifest;
+  private final Mocker mocker;
+  
+  private static Class<? extends Annotation>[] controls;
+  
+  static {
+    controls = AnnotationFinder.load("mockwire", "control");
+  }
+
+  ControlledAnnotatedFieldProcessor(IsolatedTestManifest manifest, Mocker mocker) {
+    super(controls);
+    this.manifest = manifest;
+    this.mocker = mocker;
+  }
+
+  @Override
+  public void processField(Object target, Field field) {
+    manifest.registerBean(field.getName(), mocker.mock(field.getName(), field.getType()), field.getType());
+  }
+}
