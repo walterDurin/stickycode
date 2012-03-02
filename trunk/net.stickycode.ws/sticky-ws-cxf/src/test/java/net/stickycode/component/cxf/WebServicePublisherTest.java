db3f826f39a04eaa196b329c21eef8f7d2943979
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-02 10:12:08 +0000

    Not sure why I called them component when really its ws

--- trunk/net.stickycode.ws/sticky-ws-cxf/src/test/java/net/stickycode/component/cxf/WebServicePublisherTest.java
+++ trunk/net.stickycode.ws/sticky-ws-cxf/src/test/java/net/stickycode/component/cxf/WebServicePublisherTest.java
@@ -6,14 +6,18 @@ import javax.jws.WebService;
 import net.stickycode.component.cxf.v1.DummyWebService;
 import net.stickycode.component.cxf.v1.WebServiceContract;
 import net.stickycode.mockwire.MockwireContainment;
+import net.stickycode.mockwire.Uncontrolled;
 import net.stickycode.mockwire.UnderTest;
 import net.stickycode.mockwire.junit4.MockwireRunner;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.bus.CXFBusImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(MockwireRunner.class)
-@MockwireContainment({"/META-INF/cxf/cxf.xml", "/META-INF/cxf/cxf-servlet.xml"})
+//@MockwireContainment//({"/META-INF/cxf/cxf.xml", "/META-INF/cxf/cxf-servlet.xml"})
 public class WebServicePublisherTest {
 
   @WebService
@@ -24,11 +28,16 @@ public class WebServicePublisherTest {
       implements WS {
   }
 
-  @Inject
+  @Uncontrolled
   WebServiceExposureRepository repository;
   
   @UnderTest
   WebServicePublisher publisher;
+  
+  @UnderTest
+  public Bus createBus() {
+    return BusFactory.getDefaultBus();
+  }
 
   @Test(expected = WebServiceShouldExistInVersionedPackageException.class)
   public void enforceVersionedPackages() {
