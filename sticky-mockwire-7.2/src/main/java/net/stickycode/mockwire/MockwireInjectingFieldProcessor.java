f70e7c2a14d9013dc1c528063775d24895d4aa33
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-02 09:56:05 +0000

    [Releasing sticky-mockwire] copy for tag sticky-mockwire-7.2

---
+++ sticky-mockwire-7.2/src/main/java/net/stickycode/mockwire/MockwireInjectingFieldProcessor.java
@@ -0,0 +1,22 @@
+package net.stickycode.mockwire;
+
+import java.lang.annotation.Annotation;
+
+import net.stickycode.reflector.AnnotatedFieldSettingProcessor;
+import net.stickycode.reflector.ValueSource;
+
+
+public class MockwireInjectingFieldProcessor
+    extends AnnotatedFieldSettingProcessor {
+  
+  private static Class<? extends Annotation>[] injectionMarkers;
+
+  static {
+    injectionMarkers = AnnotationFinder.load("mockwire", "injection");
+  }
+  
+  public MockwireInjectingFieldProcessor(ValueSource source) {
+    super(source, injectionMarkers);
+  }
+
+}
