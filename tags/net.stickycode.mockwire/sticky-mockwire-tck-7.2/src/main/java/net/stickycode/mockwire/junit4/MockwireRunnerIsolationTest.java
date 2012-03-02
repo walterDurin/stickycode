d1d6342fc4153edda9a60706399863b5001295b5
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-02 09:57:51 +0000

    [Releasing sticky-mockwire-tck] copy for tag sticky-mockwire-tck-7.2

---
+++ tags/net.stickycode.mockwire/sticky-mockwire-tck-7.2/src/main/java/net/stickycode/mockwire/junit4/MockwireRunnerIsolationTest.java
@@ -0,0 +1,12 @@
+package net.stickycode.mockwire.junit4;
+
+import org.junit.runner.RunWith;
+
+import net.stickycode.mockwire.AbstractIsolationTest;
+import net.stickycode.mockwire.junit4.MockwireRunner;
+
+@RunWith(MockwireRunner.class)
+public class MockwireRunnerIsolationTest
+    extends AbstractIsolationTest {
+
+}
