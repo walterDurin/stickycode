89ba1a2582256194f39935c7da06b5320ad5770b
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-07 19:21:13 +0000

    [Releasing sticky-ws-cxf-spring30] copy for tag sticky-ws-cxf-spring30-2.1

---
+++ sticky-ws-cxf-spring30-2.1/src/main/java/net/stickycode/component/cxf/spring30/WebServiceCollector.java
@@ -0,0 +1,38 @@
+package net.stickycode.component.cxf.spring30;
+
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.ws.cxf.WebServiceExposureRepository;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+@StickyComponent
+public class WebServiceCollector
+    implements BeanPostProcessor {
+
+  @Inject
+  private WebServiceExposureRepository exposures;
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName)
+      throws BeansException {
+    process(bean, beanName);
+    return bean;
+  }
+
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName)
+      throws BeansException {
+    return bean;
+  }
+
+  void process(Object bean, String beanName) {
+    for (Class<?> i : bean.getClass().getInterfaces())
+      if (i.isAnnotationPresent(WebService.class))
+        exposures.add(bean, i);
+  }
+
+}
