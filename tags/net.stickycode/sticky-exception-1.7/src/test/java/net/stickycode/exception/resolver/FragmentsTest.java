6847338347c8fa5d222d511269929cba15d7a706
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-11 03:42:07 +0000

    [Releasing sticky-exception] copy for tag sticky-exception-1.7

---
+++ tags/net.stickycode/sticky-exception-1.7/src/test/java/net/stickycode/exception/resolver/FragmentsTest.java
@@ -0,0 +1,61 @@
+package net.stickycode.exception.resolver;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class FragmentsTest {
+
+  @Test
+  public void empty() {
+    assertThat(new Fragments("")).containsOnly();
+  }
+
+  @Test
+  public void blank() {
+    assertThat(new Fragments(" ")).containsOnly(t(" "));
+    assertThat(new Fragments("    ")).containsOnly(t("    "));
+  }
+
+  @Test
+  public void word() {
+    assertThat(new Fragments("hello hello")).containsOnly(t("hello hello"));
+  }
+
+  @Test
+  public void parameters() {
+    assertThat(new Fragments("{}")).hasSize(1);
+    assertThat(new Fragments("{}{}")).hasSize(2);
+  }
+
+  @Test
+  public void quotes() {
+    assertThat(new Fragments("''")).hasSize(1);
+    assertThat(new Fragments("a''")).hasSize(2);
+    assertThat(new Fragments("a''b")).hasSize(3);
+    assertThat(new Fragments("a''")).hasSize(2);
+  }
+
+  @Test
+  public void parametersAndQuotes() {
+    assertThat(new Fragments("{}''")).hasSize(2);
+    assertThat(new Fragments("''{}")).hasSize(2);
+    assertThat(new Fragments("'' {}")).hasSize(3);
+    assertThat(new Fragments("'' {}")).containsOnly(q(), t(" "), p());
+    assertThat(new Fragments("'' {} ")).containsOnly(q(), t(" "), p(), t(" "));
+    assertThat(new Fragments("sticky '' brown {} cow")).containsOnly(t("sticky "), q(), t(" brown "), p(), t(" cow"));
+  }
+
+  private Fragment q() {
+    return new QuoteFragment();
+  }
+
+  private Fragment p() {
+    return new ParameterFragment();
+  }
+
+  private TextFragment t(String string) {
+    return new TextFragment(string);
+  }
+
+}
