5017521e113e9a3acf0659500d94dd4ba3130fc1
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-03-11 03:48:13 +0000

    Using bounds plugin during tag to mark the resolved version as the lower bound for ranges

--- trunk/net.stickycode.stile/sticky-stile/src/main/java/net/stickycode/stile/Produces.java
+++ trunk/net.stickycode.stile/sticky-stile/src/main/java/net/stickycode/stile/Produces.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Produces {
-  ResourcesTypes value();
+  ResourcesType value();
 
   // TODO? could add optional class value that is used for extensions
 }
