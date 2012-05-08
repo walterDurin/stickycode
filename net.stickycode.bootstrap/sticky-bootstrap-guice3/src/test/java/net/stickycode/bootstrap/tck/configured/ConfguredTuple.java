d5cb1a990d46d3e49ca3f7000cd0822afd981e8e
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-05-08 20:22:49 +0000

    [Releasing sticky-resource-stereotype] copy for tag sticky-resource-stereotype-1.1

--- net.stickycode.bootstrap/sticky-bootstrap-guice3/src/test/java/net/stickycode/bootstrap/tck/configured/ConfguredTuple.java
+++
@@ -1,8 +0,0 @@
-package net.stickycode.bootstrap.tck.configured;
-
-import net.stickycode.stereotype.ConfiguredComponent;
-
-@ConfiguredComponent
-public interface ConfguredTuple {
-
-}
--- net.stickycode.stile/stile-artifact-xml/src/main/java/net/stickycode/stile/artifact/xml/v1/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-    namespace = "http://schema.stickycode.net/Stile/Artifact/v1",
-    location = "http://schema.stickycode.net/Stile/Artifact-v1.xsd",
-    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.stickycode.stile.artifact.xml.v1;
-
