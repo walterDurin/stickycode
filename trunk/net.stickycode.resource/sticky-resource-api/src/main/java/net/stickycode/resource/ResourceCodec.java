d1328bd9cccf8ffd2f7d4d3ba96d675a9f0806bb
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-05-08 20:36:10 +0000

    Add some documentation and remove unneeded dependencies

--- trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceCodec.java
+++ trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceCodec.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 
 import net.stickycode.coercion.CoercionTarget;
 
+/**
+ * Contract for algorithm's that can encode and decode streams into particular targets, where a target is a field or other type.
+ */
 public interface ResourceCodec<T> {
 
   T load(InputStream source, CoercionTarget targetType);
