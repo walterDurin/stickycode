71912132c0cd44c7bec1f20e0ac59c1cc9a1a176
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-05-08 20:36:10 +0000

    Add some documentation and remove unneeded dependencies

--- net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceLocation.java
+++ net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceLocation.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 
 import net.stickycode.coercion.CoercionTarget;
 
+/**
+ * A reference to a resource and its programmatic endpoint, i.e. a path and a fields metadata where it will end up.
+ */
 public interface ResourceLocation {
 
   CoercionTarget getResourceTarget();
--- net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceProtocol.java
+++ net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceProtocol.java
@@ -3,6 +3,9 @@ package net.stickycode.resource;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * The contract for resolving in and outbound streams for resources. A representation of the wire protocol for them.
+ */
 public interface ResourceProtocol {
 
   InputStream getInputStream(ResourceLocation resourceLocation)
