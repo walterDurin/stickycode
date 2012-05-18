8277267a385866c305204d068c047a78b3cf54a0
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-05-18 05:09:18 +0000

    A component to bind the http client for scanning

---
+++ trunk/net.stickycode.resource/sticky-resource-protocol-http/src/main/java/net/stickycode/resource/protocol/http/HttpClientComponent.java
@@ -0,0 +1,11 @@
+package net.stickycode.resource.protocol.http;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import net.stickycode.stereotype.StickyComponent;
+
+@StickyComponent
+public class HttpClientComponent
+    extends DefaultHttpClient {
+
+}
