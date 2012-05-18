be6eecdf9570a4d3cff9fec228b56b2529f01877
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-05-18 04:08:16 +0000

    Adding a test to prove that multibinding to interfaces on abstract super types works

--- net.stickycode.bootstrap/sticky-bootstrap-guice3/src/test/java/net/stickycode/bootstrap/tck/plugin/PluggableTest.java
+++ net.stickycode.bootstrap/sticky-bootstrap-guice3/src/test/java/net/stickycode/bootstrap/tck/plugin/PluggableTest.java
@@ -10,22 +10,35 @@ import net.stickycode.bootstrap.tck.AbstractBootstrapTest;
 
 import org.junit.Test;
 
+import com.google.inject.Binding;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.util.Types;
+
 public class PluggableTest
     extends AbstractBootstrapTest {
 
   @Inject
   Set<Pluggable> plugged;
-  
+
+  @Inject
+  Set<PluggableContract> abstractInTheMiddle;
+
   @Inject
   Set<GenericPluggable<?>> genericPlugged;
-  
+
   @Inject
   Set<GenericPluggable> genericPluggedNoWildcard;
-  
+
+  @Inject
+  Injector injector;
+
   @Test
   public void verify() {
     assertThat(plugged).hasSize(2);
     assertThat(genericPluggedNoWildcard).hasSize(3);
     assertThat(genericPlugged).hasSize(3);
+
+    Binding bindig = injector.getBinding(Key.get(Types.setOf(PluggableContract.class)));
   }
 }
