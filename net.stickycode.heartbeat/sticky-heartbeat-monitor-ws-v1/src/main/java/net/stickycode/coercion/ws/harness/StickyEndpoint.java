ab115f1f0e6c0834e378ab795c3edc41e022217f
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-02 22:26:14 +0000

    Need to finish off the heartbeat support, all the building blocks are in place, this has been floating around uncommited for ages.

---
+++ net.stickycode.heartbeat/sticky-heartbeat-monitor-ws-v1/src/main/java/net/stickycode/coercion/ws/harness/StickyEndpoint.java
@@ -0,0 +1,14 @@
+package net.stickycode.coercion.ws.harness;
+
+
+
+public class StickyEndpoint{
+//    extends EndpointImpl {
+//
+//  public StickyEndpoint(BindingID bindingId, Object impl, WebServiceFeature[] features) {
+//    super(bindingId, impl, features);
+//  }
+//  
+//  get
+
+}
