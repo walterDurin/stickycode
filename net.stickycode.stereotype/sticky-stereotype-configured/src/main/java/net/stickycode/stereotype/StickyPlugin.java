254687a26278ef1c64cebb560ebcd17c28d93945
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-14 02:18:58 +0000

    Moving the stereotypes to a new group for a cleanup and refactoring. Separate artifacts for different stereotype areas reduces change. Can have one stereotype artifact that composes them.

--- net.stickycode.stereotype/sticky-stereotype-configured/src/main/java/net/stickycode/stereotype/StickyPlugin.java
+++ net.stickycode.stereotype/sticky-stereotype-configured/src/main/java/net/stickycode/stereotype/StickyPlugin.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -12,33 +12,21 @@
  */
 package net.stickycode.stereotype;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * <p>
- * Used to mark a method as defining a point in the application where the heartbeat can be measured
- * </p>
- * 
- * <p>
- * The schedule is of measurement is NOT defined on the method or annotation but in the configuration system of the context, this separation is
- * important
- * </p>
- * 
- * In order to ensure consistency across environments the schedule should always be defined by the environment, never by the code.
- * </p>
- */
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Pulse {
 
-  /**
-   * Describe the pulse such that when this message is presented to the user they can configure the schedule
-   * appropriately
-   */
-  String value() default "";
+/**
+ * This stereotype marks a component as being a <a href="http://martinfowler.com/eaaCatalog/plugin.html">plugin</a>.
+ * 
+ * Note that this is not meta annotated with {@link StickyComponent} as components are singular where as plugins are not.
+ * 
+ * It could be used to carry out context validation to ensure the pattern is being used appropriately.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StickyPlugin {
+
 }
