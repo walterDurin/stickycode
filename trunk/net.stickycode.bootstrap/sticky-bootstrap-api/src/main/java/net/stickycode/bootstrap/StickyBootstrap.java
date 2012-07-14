97f199e83d0ad28a3e7a36033f6bda1ba0443e86
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-14 01:58:37 +0000

    After a bit of thinking this does nice ordering of StickySystem starts and stops. In order to not have coupling between the systems the dependencies need to be resolved in terms of stereotypes.

---
+++ trunk/net.stickycode.bootstrap/sticky-bootstrap-api/src/main/java/net/stickycode/bootstrap/StickyBootstrap.java
@@ -0,0 +1,145 @@
+package net.stickycode.bootstrap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.stereotype.component.StickyExtension;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Bootstrap for the StickyCode systems, which are defined by being beans annotated by {@link StickyExtension} and implementing
+ * {@link StickySystem}.
+ */
+@StickyComponent
+public class StickyBootstrap {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  private List<StickySystem> systems;
+
+  @Inject
+  public StickyBootstrap(Set<StickySystem> subsystems) {
+    this.systems = orderByDependency(subsystems);
+    log.info("bootstrap order {}");
+  }
+
+  private List<StickySystem> orderByDependency(Set<StickySystem> subsystems) {
+    LinkedList<StickySystem> order = new LinkedList<StickySystem>(subsystems);
+    sort(order);
+    return order;
+  }
+
+  /**
+   * Sort algorithm that
+   * <ol>
+   * <li>starts at the head
+   * <li>
+   * <li>finds elements that the head uses
+   * <li>
+   * <li>bring it to front
+   * <li>
+   * <li>repeat for the new head</li>
+   * <li>if there are no deps then the current head is in the correct place</li>
+   * <li>repeat for next element</li>
+   * </ol>
+   * 
+   * A form of selection sort, the sets will always be small so efficiency not a big deal over simplicity.
+   */
+  private void sort(LinkedList<StickySystem> order) {
+    if (order.isEmpty())
+      return;
+
+    StickySystem head = order.pop();
+
+    StickySystem usedBy = findUses(head, order);
+    while (usedBy != head) {
+      order.push(head);
+      head = usedBy;
+      usedBy = findUses(head, order);
+    }
+    sort(order);
+
+    order.push(head);
+  }
+
+  private StickySystem findUses(StickySystem target, Deque<StickySystem> order) {
+    Iterator<StickySystem> i = order.iterator();
+    while (i.hasNext()) {
+      StickySystem s = i.next();
+      if (target.uses(s) && !s.uses(target)) {
+        i.remove();
+        return s;
+      }
+      if (s.isUsedBy(target) && !target.isUsedBy(s)) {
+        i.remove();
+        return s;
+      }
+
+    }
+    return target;
+  }
+
+  List<StickySystem> startOrder() {
+    return systems;
+  }
+
+  List<StickySystem> shutdownOrder() {
+    List<StickySystem> list = new ArrayList<StickySystem>(systems);
+    Collections.reverse(list);
+    return list;
+  }
+
+  public void start() {
+    for (StickySystem system : startOrder()) {
+      try {
+        system.start();
+      }
+      catch (Exception e) {
+        log.error("failed to start {}", system, e);
+      }
+    }
+  }
+
+  public void pause() {
+    for (StickySystem system : startOrder()) {
+      try {
+        system.pause();
+      }
+      catch (Exception e) {
+        log.error("failed to pause {}", system, e);
+      }
+    }
+  }
+
+  public void unpause() {
+    for (StickySystem system : shutdownOrder()) {
+      try {
+        system.unpause();
+      }
+      catch (Exception e) {
+        log.error("failed to unpause {}", system, e);
+      }
+    }
+  }
+
+  public void shutdown() {
+    for (StickySystem system : shutdownOrder()) {
+      try {
+        system.shutdown();
+      }
+      catch (Exception e) {
+        log.error("failed to stop {}", system, e);
+      }
+    }
+  }
+}
---
+++ trunk/net.stickycode.bootstrap/sticky-bootstrap-api/src/test/java/net/stickycode/bootstrap/StickyBootstrapTest.java
@@ -0,0 +1,88 @@
+package net.stickycode.bootstrap;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class StickyBootstrapTest {
+
+  private StickySystem base = new SystemStub("base");
+
+  private StickySystem usesBase = new SystemStub("usesBase").dependsOn(base);
+
+  private StickySystem usedByBase = new SystemStub("usedByBase").isDependedOnBy(base);
+
+  private StickySystem beforeAll = new SystemStub("beforeAll") {
+
+    public boolean isUsedBy(StickySystem system) {
+      return true;
+    }
+  };
+
+  private StickySystem beforeAll2 = new SystemStub("beforeAll2") {
+
+    public boolean isUsedBy(StickySystem system) {
+      return true;
+    }
+  };
+
+  @Test
+  public void same() {
+    assertThat(ordered(base)).containsExactly(base);
+  }
+
+  @Test
+  public void uses() {
+    assertThat(ordered(base, usesBase)).containsExactly(base, usesBase);
+    assertThat(ordered(usesBase, base)).containsExactly(base, usesBase);
+  }
+
+  @Test
+  public void usedBy() {
+    assertThat(ordered(usedByBase, base)).containsExactly(usedByBase, base);
+    assertThat(ordered(base, usedByBase)).containsExactly(usedByBase, base);
+  }
+
+  @Test
+  public void usedByAndUses() {
+    assertThat(ordered(usesBase, base, usedByBase)).containsExactly(usedByBase, base, usesBase);
+    assertThat(ordered(base, usesBase, usedByBase)).containsExactly(usedByBase, base, usesBase);
+    assertThat(ordered(usedByBase, usesBase, base)).containsExactly(usedByBase, base, usesBase);
+  }
+
+  @Test
+  public void before() {
+    assertThat(ordered(beforeAll, beforeAll2))
+        .containsExactly(beforeAll, beforeAll2);
+  }
+
+  @Test
+  public void beforeAll() {
+    assertThat(ordered(base, usedByBase, usesBase, beforeAll))
+        .containsExactly(beforeAll, usedByBase, base, usesBase);
+  }
+
+  @Test
+  public void beforeAllx2() {
+    assertThat(ordered(base, usedByBase, beforeAll2, usesBase, beforeAll))
+        .containsExactly(beforeAll2, beforeAll, usedByBase, base, usesBase);
+  }
+
+  @Test
+  public void shutdownBeforeAllx2() {
+    assertThat(shutdownOrder(base, usedByBase, beforeAll2, usesBase, beforeAll))
+        .containsExactly(usesBase, base, usedByBase, beforeAll2, beforeAll);
+  }
+
+  private List<StickySystem> ordered(StickySystem... base2) {
+    StickyBootstrap bootstrap = new StickyBootstrap(new ArraySet<StickySystem>(base2));
+    return bootstrap.startOrder();
+  }
+
+  private List<StickySystem> shutdownOrder(StickySystem... base2) {
+    StickyBootstrap bootstrap = new StickyBootstrap(new ArraySet<StickySystem>(base2));
+    return bootstrap.shutdownOrder();
+  }
+}
