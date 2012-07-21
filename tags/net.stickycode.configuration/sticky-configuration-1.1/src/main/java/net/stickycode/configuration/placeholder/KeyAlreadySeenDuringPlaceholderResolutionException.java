e906c86ada464f01d14841581c3ca03f8848efd6
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-21 11:39:20 +0000

    [Releasing sticky-configuration] copy for tag sticky-configuration-1.1

---
+++ tags/net.stickycode.configuration/sticky-configuration-1.1/src/main/java/net/stickycode/configuration/placeholder/KeyAlreadySeenDuringPlaceholderResolutionException.java
@@ -0,0 +1,13 @@
+package net.stickycode.configuration.placeholder;
+
+import net.stickycode.exception.PermanentException;
+
+@SuppressWarnings("serial")
+public class KeyAlreadySeenDuringPlaceholderResolutionException
+    extends PermanentException {
+
+  public KeyAlreadySeenDuringPlaceholderResolutionException(Placeholder placeholder, ResolvedValue resolution) {
+    super("Encountered placeholder {} again when resolving {}", placeholder, resolution);
+  }
+
+}
---
+++ tags/net.stickycode.configuration/sticky-configuration-1.1/src/test/java/net/stickycode/configuration/SystemPropertiesConfigurationSourceTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.configuration;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+
+public class SystemPropertiesConfigurationSourceTest {
+
+  @Test
+  public void has() {
+    assertThat(apply("java.home").hasValue()).isTrue();
+    assertThat(apply("user.dir").hasValue()).isTrue();
+    assertThat(apply("no.such.property").hasValue()).isFalse();
+  }
+
+  private LookupValues apply(String key) {
+    LookupValues values = new LookupValues();
+    source().apply(new PlainConfigurationKey(key), values);
+    return values;
+  }
+
+  @Test
+  public void get() {
+    assertThat(apply("java.home").getValue()).isNotNull();
+  }
+
+  private SystemPropertiesConfigurationSource source() {
+    return new SystemPropertiesConfigurationSource();
+  }
+
+}
