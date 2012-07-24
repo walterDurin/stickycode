51a6c56a28cefc16744cc58b4e5ccae1f7792dbe
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-24 07:53:39 +0000

    [Releasing sticky-coercion] copy for tag sticky-coercion-5.2

---
+++ sticky-coercion-5.2/src/test/java/net/stickycode/coercion/InetSocketAddressCoercionTest.java
@@ -0,0 +1,65 @@
+package net.stickycode.coercion;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.net.InetSocketAddress;
+import java.util.regex.Pattern;
+
+import net.stickycode.coercion.target.CoercionTargets;
+
+import org.junit.Test;
+
+public class InetSocketAddressCoercionTest {
+
+  @Test
+  public void applicability() {
+    assertThat(new InetSocketAddressCoercion().isApplicableTo(coercionTarget())).isTrue();
+    assertThat(new InetSocketAddressCoercion().isApplicableTo(CoercionTargets.find(String.class))).isFalse();
+    assertThat(new InetSocketAddressCoercion().isApplicableTo(CoercionTargets.find(Pattern.class))).isFalse();
+  }
+
+  private CoercionTarget coercionTarget() {
+    return CoercionTargets.find(InetSocketAddress.class);
+  }
+
+  @Test(expected = InetSocketAddressSpecificationInvalidException.class)
+  public void blankCheck() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "");
+  }
+
+  @Test(expected = InetSocketAddressSpecificationInvalidException.class)
+  public void noPort() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "localhost");
+  }
+
+  @Test(expected = InetSocketAddressSpecificationInvalidException.class)
+  public void noPortAfterColon() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "localhost:");
+  }
+  
+  @Test(expected = InetSocketAddressCannotBeResolvedFromHostNameException.class)
+  public void unresolvableAddress() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "doesnotexistanywhere.:70");
+  }
+  
+  @Test(expected = InetSocketAddressCannotBeResolvedFromHostNameException.class)
+  public void madeUpAddress() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "doesn=ot+ ex&istanywhere.:70");
+  }
+  
+  @Test
+  public void local() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "127.0.0.1:70");
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "::1:70");
+  }
+  
+  @Test
+  public void localhost() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "localhost:70");
+  }
+  
+  @Test
+  public void ipv6() {
+    new InetSocketAddressCoercion().coerce(coercionTarget(), "fe80::21a:73ff:fe88:e216:70");
+  }
+}
