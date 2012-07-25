56204e348610f7e4589574fb82bbbf404f13ca4b
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-07-25 08:40:01 +0000

    Make sure to safely shutdown the executor

--- net.stickycode.scheduled/sticky-scheduler-single/src/main/java/net/stickycode/scheduled/single/StickyThreadPoolExcutor.java
+++ net.stickycode.scheduled/sticky-scheduler-single/src/main/java/net/stickycode/scheduled/single/StickyThreadPoolExcutor.java
@@ -2,29 +2,12 @@ package net.stickycode.scheduled.single;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 public class StickyThreadPoolExcutor
     extends ScheduledThreadPoolExecutor {
 
-  private Logger log = LoggerFactory.getLogger(getClass());
-  
   public StickyThreadPoolExcutor(int corePoolSize) {
     super(corePoolSize, new NamedThreadFactory("sticky"));
-  }
-
-  @Override
-  protected void beforeExecute(Thread t, Runnable r) {
-    log.debug("executing {} on {}", r, t);
-    super.beforeExecute(t, r);
-  }
-  
-  @Override
-  protected void afterExecute(Runnable r, Throwable t) {
-    super.afterExecute(r, t);
-    log.debug("executed {}", r, t);
   }
 
 }
