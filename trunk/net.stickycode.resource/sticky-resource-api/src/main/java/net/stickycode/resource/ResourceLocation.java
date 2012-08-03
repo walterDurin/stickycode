2ab06a720213902102c0c92ccffbcaafdbe80e8f
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-08-03 11:49:54 +0000

    Introducing the concept of the resource connection - need it to deal with http and other remote transports... WIP will isolate the concepts and trim it down soon

--- trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceLocation.java
+++ trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceLocation.java
@@ -1,8 +1,5 @@
 package net.stickycode.resource;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import net.stickycode.coercion.CoercionTarget;
 
 /**
@@ -12,10 +9,8 @@ public interface ResourceLocation {
 
   CoercionTarget getResourceTarget();
 
-  InputStream getInputStream();
-
-  OutputStream getOutputStream();
-
   String getPath();
 
+  String getScheme();
+  
 }
--- trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceProtocol.java
+++ trunk/net.stickycode.resource/sticky-resource-api/src/main/java/net/stickycode/resource/ResourceProtocol.java
@@ -1,18 +1,14 @@
 package net.stickycode.resource;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-
 /**
- * The contract for resolving in and outbound streams for resources. A representation of the wire protocol for them.
+ * A {@link ResourceProtocol} represents a wire protocol for a resource, it can creation {@link ResourceConnection}s to
+ * resources.
  */
 public interface ResourceProtocol {
 
-  InputStream getInputStream(ResourceLocation resourceLocation)
+  <T> ResourceConnection<T> createConnection(ResourceLocation resourceLocation)
       throws ResourceNotFoundException;
 
   boolean canResolve(String protocol);
-
-  OutputStream getOutputStream(ResourceLocation uriResourceLocation);
 
 }
