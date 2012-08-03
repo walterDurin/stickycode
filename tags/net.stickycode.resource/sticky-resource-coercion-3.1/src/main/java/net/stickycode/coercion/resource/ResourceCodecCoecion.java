04d9358ff29a8765c68586f004e875b29711d106
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-08-03 12:09:40 +0000

    [Releasing sticky-resource-coercion] copy for tag sticky-resource-coercion-3.1

---
+++ tags/net.stickycode.resource/sticky-resource-coercion-3.1/src/main/java/net/stickycode/coercion/resource/ResourceCodecCoecion.java
@@ -0,0 +1,38 @@
+package net.stickycode.coercion.resource;
+
+import javax.inject.Inject;
+
+import net.stickycode.coercion.AbstractNoDefaultCoercion;
+import net.stickycode.coercion.CoercionTarget;
+import net.stickycode.resource.ResourceCodec;
+import net.stickycode.resource.ResourceCodecRegistry;
+import net.stickycode.stereotype.plugin.StickyExtension;
+
+@StickyExtension
+public class ResourceCodecCoecion<T>
+    extends AbstractNoDefaultCoercion<ResourceCodec<T>> {
+
+  @Inject
+  private ResourceCodecRegistry codecs;
+
+  @Override
+  public ResourceCodec<T> coerce(CoercionTarget target, String value) {
+    return getDefaultValue(target);
+  }
+
+  @Override
+  public boolean isApplicableTo(CoercionTarget target) {
+    return target.getType().isAssignableFrom(ResourceCodec.class);
+  }
+
+  @Override
+  public boolean hasDefaultValue() {
+    return true;
+  }
+
+  @Override
+  public ResourceCodec<T> getDefaultValue(CoercionTarget target) {
+    return codecs.find(target.getComponentCoercionTypes()[0]);
+  }
+
+}
