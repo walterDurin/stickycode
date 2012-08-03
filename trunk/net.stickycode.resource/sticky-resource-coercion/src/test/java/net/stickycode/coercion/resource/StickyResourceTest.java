8c5765877e3489ad86fc3533f076162c137eedd0
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-08-03 12:09:17 +0000

    Stale test its rather difficult to emulate the lazy loaded resource, the sticky resource test does it properly

--- trunk/net.stickycode.resource/sticky-resource-coercion/src/test/java/net/stickycode/coercion/resource/StickyResourceTest.java
+++ trunk/net.stickycode.resource/sticky-resource-coercion/src/test/java/net/stickycode/coercion/resource/StickyResourceTest.java
@@ -5,11 +5,8 @@ import static org.fest.assertions.Assertions.assertThat;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import net.stickycode.coercion.CoercionTarget;
-import net.stickycode.coercion.target.CoercionTargets;
 import net.stickycode.mockwire.MockwireConfigured;
 import net.stickycode.mockwire.junit4.MockwireRunner;
-import net.stickycode.reflector.Fields;
 import net.stickycode.stereotype.configured.Configured;
 import net.stickycode.stereotype.resource.Resource;
 
@@ -36,16 +33,6 @@ public class StickyResourceTest {
   public void configured() {
     assertThat(bean).isNotNull();
     assertThat(bean.get().value).isEqualTo("loaded");
-  }
-  
-  @Test
-  public void local() {
-    assertThat(bean).isNotNull();
-    assertThat(bean.get().value).isEqualTo("loaded");
-  }
-  
-  private CoercionTarget target() {
-    return CoercionTargets.find(Fields.find(getClass(), "bean"));
   }
 
 }
