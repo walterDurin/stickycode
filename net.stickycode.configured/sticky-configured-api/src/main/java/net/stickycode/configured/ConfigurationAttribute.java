66b83e9b8803c89b3e325c575005742163535ed1
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-08-03 11:32:35 +0000

    Turns out recurse on attribute is not needed, invertingControl is enough to register configured attributes of children

--- net.stickycode.configured/sticky-configured-api/src/main/java/net/stickycode/configured/ConfigurationAttribute.java
+++ net.stickycode.configured/sticky-configured-api/src/main/java/net/stickycode/configured/ConfigurationAttribute.java
@@ -27,64 +27,12 @@ import net.stickycode.configuration.ResolvedConfiguration;
 public interface ConfigurationAttribute
     extends ConfigurationTarget {
 
-//  /**
-//   * Return true if there was a default value defined at development time.
-//   * 
-//   * @return true if there is a default value for the attribute
-//   */
-//  boolean hasDefaultValue(); 
-//
-//  /**
-//   * The default value of the attribute as defined at development time.
-//   * 
-//   * @return The default value of the attribute as defined at development time.
-//   */
-//  Object getDefaultValue();
-//
-//  /**
-//   * Set the underlying attribute.
-//   * 
-//   * @param value The new value of the attribute
-//   */
-//  void setValue(Object value);
-//
-//  /**
-//   * Return the name of the underlying attribute
-//   * 
-//   * @return The name of the attribute
-//   */
-//  String getName();
-//
-//  /**
-//   * Return the current value of the configured attribute
-//   */
-//  Object getValue();
-//
-//  /**
-//   * Return the metadata required to coerce this attribute
-//   */
-//  CoercionTarget getCoercionTarget();
-//  
-//  /**
-//   * Return true if this attribute can be updated, i.e. setValue can be called.
-//   * 
-//   * You might use this as false for attributes that start with a known value and cannot be updated.
-//   */
-//  boolean canBeUpdated();
-//
-//  /**
-//   * Return true if this attribute has a value, either a default value or one thats been set
-//   */
-//  boolean hasValue();
-
   void applyCoercion(CoercionFinder coercions);
 
   void update();
 
   void invertControl(ComponentContainer container);
   
-  void recurse(ConfigurationMetadataProcessor processor);
-
   boolean requiresResolution();
   
   ResolvedConfiguration getResolution();
