8540b4b83ac95d51e99e90447997f5c386579ba6
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-08-07 11:09:46 +0000

    Releasing sticky-mockwire at 8.2-SNAPSHOT … copy for tag sticky-mockwire-8.2

---
+++ tags/net.stickycode.mockwire/sticky-mockwire-8.2/src/main/java/net/stickycode/mockwire/PomUtils.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.mockwire;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+
+
+public class PomUtils {
+
+  public static String loadVersion(String groupId, String artifactId) {
+    URL url = PomUtils.class.getResource("/META-INF/maven/" + groupId + "/" + artifactId + "/pom.properties");
+    if (url == null)
+      return "SNAPSHOT";
+
+    Properties p = new Properties();
+    try {
+      InputStream in = url.openStream();
+      p.load(in);
+      return p.getProperty("version");
+    }
+    catch (IOException e) {
+      throw new RuntimeException("Failed to load " + url, e);
+    }
+  }
+
+}
