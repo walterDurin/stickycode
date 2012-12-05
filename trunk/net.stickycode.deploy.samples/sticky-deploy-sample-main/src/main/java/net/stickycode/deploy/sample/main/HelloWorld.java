be5c34f1530e6def28be5f04dcf63b33f493c5c5
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-12-05 09:39:31 +0000

    Allow for passing parameters and delaying the run for analysis

--- trunk/net.stickycode.deploy.samples/sticky-deploy-sample-main/src/main/java/net/stickycode/deploy/sample/main/HelloWorld.java
+++ trunk/net.stickycode.deploy.samples/sticky-deploy-sample-main/src/main/java/net/stickycode/deploy/sample/main/HelloWorld.java
@@ -3,6 +3,21 @@ package net.stickycode.deploy.sample.main;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    if (args.length == 0)
+      System.out.println("Hello World!");
+
+    for (int i = 0; i < args.length; i++)
+      try {
+        for (int j = 0; j < Integer.parseInt(args[i]); j++) {
+          System.out.println("Hello world (" + i + "," + j + ")!");
+          Thread.sleep(i * 1000);
+        }
+      }
+      catch (NumberFormatException e) {
+        System.err.println("This is not a number " + args[i]);
+      }
+      catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
   }
 }
