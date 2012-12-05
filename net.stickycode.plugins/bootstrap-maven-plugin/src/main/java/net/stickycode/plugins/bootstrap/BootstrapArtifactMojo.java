34d518cb665db8d1e622e47dad19a078cc37157c
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-12-05 09:41:29 +0000

    Rename the plugins to artifact and project as thats more reflective of what they do, will add an artifacts next to include more than one project

--- net.stickycode.plugins/bootstrap-maven-plugin/src/main/java/net/stickycode/plugins/bootstrap/BootstrapArtifactMojo.java
+++ net.stickycode.plugins/bootstrap-maven-plugin/src/main/java/net/stickycode/plugins/bootstrap/BootstrapArtifactMojo.java
@@ -3,25 +3,52 @@ package net.stickycode.plugins.bootstrap;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Set;
 
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
 
-@Mojo(name = "artifact", requiresDependencyResolution = ResolutionScope.RUNTIME)
+@Mojo(name = "artifact")
 public class BootstrapArtifactMojo
     extends AbstractBootstrapMojo {
 
+  @Parameter(required = true)
+  private String groupId;
+
+  @Parameter(required = true)
+  private String artifactId;
+
+  @Parameter(required = true)
+  private String version;
+
+  @Parameter(defaultValue = "jar")
+  private String extension;
+
+  @Parameter(defaultValue = "")
+  private String classifier;
+
   @Override
   protected Collection<File> collectArtifacts() {
-    Set<Artifact> artifacts = getProject().getArtifacts();
-    Collection<File> files = new ArrayList<File>();
-    for (Artifact artifact : artifacts) {
-      files.add(artifact.getFile());
-      getLog().info("adding " + artifact);
+    Artifact artifact = artifactForLookup();
+    Collection<Artifact> artifacts = collectArtifacts(artifact);
+    Collection<File> files = new ArrayList<File>(artifacts.size());
+    for (Artifact a : artifacts) {
+      files.add(a.getFile());
+      getLog().info("adding " + a);
     }
     return files;
+  }
+
+  Artifact artifactForLookup() {
+    Artifact artifact = new DefaultArtifact(
+        groupId,
+        artifactId,
+        classifier,
+        extension,
+        version);
+    getLog().info("Creating bootstrap from " + artifact);
+    return artifact;
   }
 
 }
