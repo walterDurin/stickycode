ecd700c7959c60e13ae77e64964df3343f34e717
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2012-12-06 10:16:37 +0000

    Add the project file

--- net.stickycode.plugins/bootstrap-maven-plugin/src/main/java/net/stickycode/plugins/bootstrap/BootstrapProjectMojo.java
+++ net.stickycode.plugins/bootstrap-maven-plugin/src/main/java/net/stickycode/plugins/bootstrap/BootstrapProjectMojo.java
@@ -17,6 +17,10 @@ public class BootstrapProjectMojo
   protected Collection<File> collectArtifacts() {
     Set<Artifact> artifacts = getProject().getArtifacts();
     Collection<File> files = new ArrayList<File>();
+
+    files.add(getProject().getArtifact().getFile());
+    getLog().info("adding " + getProject().getArtifact());
+    
     for (Artifact artifact : artifacts) {
       files.add(artifact.getFile());
       getLog().info("adding " + artifact);
