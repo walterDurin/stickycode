76ab442cd1688067131dd73c7e3e24fda391702c
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2014-03-26 10:28:56 +0000

    Releasing sticky-configured at 5.5-SNAPSHOT … copy for tag sticky-configured-5.5

---
+++ tags/net.stickycode.configured/sticky-configured-5.5/src/test/java/net/stickycode/configured/ConfiguredTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.configured;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import net.stickycode.stereotype.configured.Configured;
+
+import org.junit.Test;
+
+public class ConfiguredTest {
+
+  @Configured
+  String configured;
+
+  @Configured
+  Integer number;
+
+  @Configured
+  List<Double> doubles;
+
+  String notConfigured;
+
+  @Test
+  public void notConfigured() {
+    assertThat(canProcess("notConfigured")).isFalse();
+  }
+
+  @Test
+  public void configured() {
+    assertThat(canProcess("configured")).isTrue();
+  }
+
+  @Test
+  public void configuredNumber() {
+    assertThat(canProcess("number")).isTrue();
+  }
+
+  @Test
+  public void configuredDoubles() {
+    assertThat(canProcess("doubles")).isTrue();
+  }
+
+  private boolean canProcess(String fieldName) {
+    ConfiguredFieldProcessor configuredFieldProcessor = new ConfiguredFieldProcessor(null, null);
+    return configuredFieldProcessor.canProcess(getField(fieldName));
+  }
+
+  private Field getField(String fieldName) {
+    try {
+      return getClass().getDeclaredField(fieldName);
+    }
+    catch (SecurityException e) {
+      throw new RuntimeException(e);
+    }
+    catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
