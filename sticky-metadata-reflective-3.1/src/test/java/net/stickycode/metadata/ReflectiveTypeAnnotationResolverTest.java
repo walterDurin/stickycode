10966e1c53fdd298a9b1b295ce6bee2ab40e5f3b
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-05 20:34:06 +0000

    Releasing sticky-metadata-reflective at 3.1-SNAPSHOT …copy for tag sticky-metadata-reflective-3.1

---
+++ sticky-metadata-reflective-3.1/src/test/java/net/stickycode/metadata/ReflectiveTypeAnnotationResolverTest.java
@@ -0,0 +1,65 @@
+package net.stickycode.metadata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.stereotype.StickyFramework;
+import net.stickycode.stereotype.StickyPlugin;
+import net.stickycode.stereotype.component.StickyRepository;
+import net.stickycode.stereotype.plugin.StickyExtension;
+
+import org.junit.Test;
+
+public class ReflectiveTypeAnnotationResolverTest {
+
+  @StickyComponent
+  public interface AnnotatedContract {
+  }
+
+  public class WithAnnotatedContract
+      implements AnnotatedContract {
+  }
+
+  public class WithInheritedAnnotatedContract
+      extends WithAnnotatedContract {
+  }
+
+  @StickyComponent
+  public class WithDirectContractAnnotation {
+  }
+
+  @Test
+  public void annotated() {
+    assertThat(type(AnnotatedContract.class).annotatedWith(StickyPlugin.class)).isFalse();
+    assertThat(type(AnnotatedContract.class).annotatedWith(StickyExtension.class)).isFalse();
+    assertThat(type(AnnotatedContract.class).annotatedWith(StickyRepository.class)).isFalse();
+    assertThat(type(AnnotatedContract.class).annotatedWith(StickyComponent.class)).isTrue();
+  }
+
+  @Test
+  public void contract() {
+    assertThat(type(WithAnnotatedContract.class).annotatedWith(StickyPlugin.class)).isFalse();
+    assertThat(type(WithAnnotatedContract.class).annotatedWith(StickyExtension.class)).isFalse();
+    assertThat(type(WithAnnotatedContract.class).annotatedWith(StickyRepository.class)).isFalse();
+    assertThat(type(WithAnnotatedContract.class).annotatedWith(StickyComponent.class)).isTrue();
+  }
+
+  @Test
+  public void direct() {
+    assertThat(type(WithDirectContractAnnotation.class).annotatedWith(StickyFramework.class)).isFalse();
+    assertThat(type(WithDirectContractAnnotation.class).annotatedWith(StickyPlugin.class)).isFalse();
+    assertThat(type(WithDirectContractAnnotation.class).annotatedWith(StickyExtension.class)).isFalse();
+    assertThat(type(WithDirectContractAnnotation.class).annotatedWith(StickyComponent.class)).isTrue();
+  }
+
+  @Test
+  public void inheritedContract() {
+    assertThat(type(WithInheritedAnnotatedContract.class).annotatedWith(StickyFramework.class)).isFalse();
+    assertThat(type(WithInheritedAnnotatedContract.class).annotatedWith(StickyPlugin.class)).isFalse();
+    assertThat(type(WithInheritedAnnotatedContract.class).annotatedWith(StickyExtension.class)).isFalse();
+    assertThat(type(WithInheritedAnnotatedContract.class).annotatedWith(StickyComponent.class)).isTrue();
+  }
+
+  private MetadataResolver type(Class<?> type) {
+    return new ReflectiveTypeMetadataResolver(type);
+  }
+}
