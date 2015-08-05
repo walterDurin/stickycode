c8463de04a7360bc73f50f34bb6a1bbcc1fba1b3
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-05 11:17:53 +0000

    had this sitting around for a few years, should reconstitute it soon

--- net.stickycode/sticky-resource-0/src/test/java/net/stickycode/util/LinkedIteratorTest.java
+++ net.stickycode/sticky-resource-0/src/test/java/net/stickycode/util/LinkedIteratorTest.java
@@ -1,6 +1,6 @@
 package net.stickycode.util;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import net.stickycode.util.LinkedIterator;
 
