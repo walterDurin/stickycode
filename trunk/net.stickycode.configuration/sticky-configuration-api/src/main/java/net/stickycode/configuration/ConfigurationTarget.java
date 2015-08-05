b294d4100853868825b9e3eed4ed5230dc1d8b92
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-05 20:45:06 +0000

    old doc updates

--- trunk/net.stickycode.configuration/sticky-configuration-api/src/main/java/net/stickycode/configuration/ConfigurationTarget.java
+++ trunk/net.stickycode.configuration/sticky-configuration-api/src/main/java/net/stickycode/configuration/ConfigurationTarget.java
@@ -2,11 +2,20 @@ package net.stickycode.configuration;
 
 import net.stickycode.coercion.CoercionTarget;
 
+/**
+ * A target that can be configured.
+ */
 public interface ConfigurationTarget
     extends ConfigurationKey {
 
+  /**
+   * Add a resolution path
+   */
   void resolvedWith(ResolvedConfiguration resolved);
 
+  /**
+   * @return the type information for the target of configuration.
+   */
   CoercionTarget getCoercionTarget();
 
 }
