77894a7e441d868e25849a1dcdc1780882c90376
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-06 10:29:54 +0000

    Releasing sticky-configuration at 2.1-SNAPSHOT …copy for tag sticky-configuration-2.1

---
+++ sticky-configuration-2.1/src/test/java/net/stickycode/configuration/source/FilePropertiesConfigurationSourceTest.java
@@ -0,0 +1,49 @@
+package net.stickycode.configuration.source;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class FilePropertiesConfigurationSourceTest {
+
+  @Test
+  public void pathNotSet() {
+    System.clearProperty("configuration.path");
+    new FilePropertiesConfigurationSource().loadApplicationConfiguration();
+  }
+  
+  @Test
+  public void pathSetToMultipleFilesLoadedInOrderGiven() {
+    System.setProperty("configuration.path", "src/test/resources/config.d/01first.properties,src/test/resources/config.d/02second.properties");
+    FilePropertiesConfigurationSource source = new FilePropertiesConfigurationSource();
+    source.loadApplicationConfiguration();
+    assertThat(source.getValue("first.is")).isEqualTo("second");
+    assertThat(source.getValue("second.is")).isEqualTo("second");
+  }
+
+  @Test(expected = ConfigurationFileNotFoundException.class)
+  public void pathNotFound() {
+    System.setProperty("configuration.path", "src/test/notresources");
+    new FilePropertiesConfigurationSource().loadApplicationConfiguration();
+  }
+
+  @Test
+  public void directoryWithNoConfigFiles() {
+    System.setProperty("configuration.path", "src/test/resources");
+    FilePropertiesConfigurationSource source = new FilePropertiesConfigurationSource();
+    source.loadApplicationConfiguration();
+    assertThat(source.size()).isEqualTo(0);
+  }
+
+  @Test
+  public void directoryWithConfigFiles() {
+    System.setProperty("configuration.path", "src/test/resources/config.d");
+    FilePropertiesConfigurationSource source = new FilePropertiesConfigurationSource();
+    source.loadApplicationConfiguration();
+    assertThat(source.size()).isEqualTo(3);
+
+    assertThat(source.getValue("first.is")).isEqualTo("first");
+    assertThat(source.getValue("second.is")).isEqualTo("second");
+    assertThat(source.getValue("third.is")).isEqualTo("third");
+  }
+}
