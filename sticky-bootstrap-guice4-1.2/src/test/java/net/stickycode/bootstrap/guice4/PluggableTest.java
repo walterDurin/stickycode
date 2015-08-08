3b0f173dc18e97ec47cd7ede2b40fc8817e1f991
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-08 13:24:27 +0000

    Releasing sticky-bootstrap-guice4 at 1.2-SNAPSHOT …copy for tag sticky-bootstrap-guice4-1.2

---
+++ sticky-bootstrap-guice4-1.2/src/test/java/net/stickycode/bootstrap/guice4/PluggableTest.java
@@ -0,0 +1,32 @@
+package net.stickycode.bootstrap.guice4;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.util.Types;
+
+import net.stickycode.bootstrap.guice3.StickyGuice;
+import net.stickycode.bootstrap.tck.plugin.AbstractPluggableTest;
+import net.stickycode.bootstrap.tck.plugin.PluggableContract;
+
+public class PluggableTest
+    extends AbstractPluggableTest {
+
+  @Before
+  public void setup() {
+    Injector injector = StickyGuice.createApplicationInjector(getClass().getPackage().getName());
+    injector.injectMembers(this);
+  }
+
+  @Inject
+  Injector injector;
+
+  @Test
+  public void keys() {
+    injector.getBinding(Key.get(Types.setOf(PluggableContract.class)));
+  }
+}
