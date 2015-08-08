0abd45748d6d51bb6431864aad4a1e29f977258f
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-08 11:13:50 +0000

    Releasing sticky-bootstrap-guice4 at 1.1-SNAPSHOT …copy for tag sticky-bootstrap-guice4-1.1

---
+++ sticky-bootstrap-guice4-1.1/src/test/java/net/stickycode/bootstrap/tck/component/ComponentTest.java
@@ -0,0 +1,42 @@
+package net.stickycode.bootstrap.tck.component;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+
+import net.stickycode.bootstrap.tck.AbstractBootstrapTest;
+
+public class ComponentTest
+    extends AbstractBootstrapTest {
+
+  @Inject
+  StandardComponent component;
+
+  @Inject
+  StandardComponent component2;
+
+  @Inject
+  TheSubInterface implementation;
+
+  @Inject
+  SubInterface contract;
+
+  @Inject
+  SubInterface contractTwo;
+
+  @Inject
+  UpTheTop upTop;
+
+  @Test
+  public void implementationAndContractAreTheSameInstance() {
+    assertThat(implementation).isSameAs(contract);
+    assertThat(implementation).isSameAs(contractTwo);
+  }
+
+  @Test
+  public void componentsScannedAreInjected() {
+    assertThat(component).isSameAs(component2);
+  }
+}
---
+++ sticky-bootstrap-guice4-1.1/src/test/java/net/stickycode/bootstrap/tck/configured/ConfiguredTest.java
@@ -0,0 +1,30 @@
+package net.stickycode.bootstrap.tck.configured;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.inject.Inject;
+
+import net.stickycode.bootstrap.tck.AbstractBootstrapTest;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ConfiguredTest
+    extends AbstractBootstrapTest {
+
+  @Inject
+  AConfiguredTuple tuple;
+
+  @Inject
+  AConfiguredTuple tuple2;
+
+  @Inject
+  ConfguredTuple tupleContract;
+
+  @Test
+  @Ignore("I think configured tuples will disappear soon, they current method of configuration is broken")
+  public void verify() {
+    assertThat(tuple).isNotSameAs(tupleContract);
+    assertThat(tuple).isNotSameAs(tuple2);
+  }
+}
