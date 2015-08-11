c58e4bc4e37b956ad11a08aebd0a2dc9ad45b27b
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-11 11:43:09 +0000

    Releasing sticky-configured-spring4 at 1.1-SNAPSHOT …copy for tag sticky-configured-spring4-1.1

---
+++ sticky-configured-spring4-1.1/src/main/java/net/stickycode/configured/spring3/ConfiguredBeanPostProcessor.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.configured.spring3;
+
+import javax.inject.Inject;
+
+import net.stickycode.configured.ConfiguredBeanProcessor;
+import net.stickycode.metadata.MetadataResolverRegistry;
+import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.stereotype.configured.Configured;
+import net.stickycode.stereotype.configured.ConfiguredStrategy;
+import net.stickycode.stereotype.configured.PostConfigured;
+import net.stickycode.stereotype.configured.PreConfigured;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
+
+@StickyComponent
+public class ConfiguredBeanPostProcessor
+    extends InstantiationAwareBeanPostProcessorAdapter {
+
+  @Inject
+  private ConfiguredBeanProcessor processor;
+
+  @Inject
+  MetadataResolverRegistry metdataResolverRegistry;
+
+  @Override
+  public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+    if (typeIsConfigured(bean.getClass())) {
+      processor.process(bean);
+    }
+    return true;
+  }
+
+  private boolean typeIsConfigured(Class<?> type) {
+    if (metdataResolverRegistry
+        .does(type)
+        .haveAnyFieldsMetaAnnotatedWith(Configured.class, ConfiguredStrategy.class))
+      return true;
+
+    if (metdataResolverRegistry
+        .does(type)
+        .haveAnyMethodsMetaAnnotatedWith(PreConfigured.class, PostConfigured.class))
+      return true;
+
+    return false;
+  }
+
+}
---
+++ sticky-configured-spring4-1.1/src/main/java/net/stickycode/configured/spring3/SpringStrategyFinder.java
@@ -0,0 +1,30 @@
+package net.stickycode.configured.spring3;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import net.stickycode.configured.strategy.StrategyFinder;
+import net.stickycode.configured.strategy.StrategyNotFoundException;
+import net.stickycode.stereotype.StickyComponent;
+
+import org.springframework.context.ApplicationContext;
+
+@StickyComponent
+public class SpringStrategyFinder
+    implements StrategyFinder {
+
+  @Inject
+  ApplicationContext context;
+
+  @Override
+  public Object findWithName(Class<?> contract, String name) throws StrategyNotFoundException {
+    Map<String, ?> m = context.getBeansOfType(contract);
+    Object o = m.get(name);
+    if (o != null)
+      return o;
+
+    throw new StrategyNotFoundException(contract, name, m.keySet());
+  }
+
+}
