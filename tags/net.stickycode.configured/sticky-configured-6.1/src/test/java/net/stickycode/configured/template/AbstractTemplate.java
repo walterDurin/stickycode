72001a10cedd3a983e996dfa5c6d011fcc830da2
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-11 11:29:35 +0000

    Releasing sticky-configured at 6.1-SNAPSHOT …copy for tag sticky-configured-6.1

---
+++ tags/net.stickycode.configured/sticky-configured-6.1/src/test/java/net/stickycode/configured/template/AbstractTemplate.java
@@ -0,0 +1,11 @@
+package net.stickycode.configured.template;
+
+import net.stickycode.stereotype.configured.Configured;
+
+
+public class AbstractTemplate {
+  
+  @Configured
+  private Integer value;
+
+}
