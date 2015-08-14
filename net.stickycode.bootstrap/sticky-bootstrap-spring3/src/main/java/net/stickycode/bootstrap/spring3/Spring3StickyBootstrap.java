4aa40701b94c072f87e626417ea34210bb5c3b99
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-14 12:43:09 +0000

    add support for domain classes as prototype
    put the 3 and 4 in the names for clarity when resolving classes in classpath, generally you will never have more than one but its easy for me to dev/test

--- net.stickycode.bootstrap/sticky-bootstrap-spring3/src/main/java/net/stickycode/bootstrap/spring3/Spring3StickyBootstrap.java
+++ net.stickycode.bootstrap/sticky-bootstrap-spring3/src/main/java/net/stickycode/bootstrap/spring3/Spring3StickyBootstrap.java
@@ -1,32 +1,33 @@
 package net.stickycode.bootstrap.spring3;
 
-import java.awt.Component;
 
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.stereotype.Component;
 
 import net.stickycode.bootstrap.StickyBootstrap;
 import net.stickycode.stereotype.StickyComponent;
+import net.stickycode.stereotype.StickyDomain;
 import net.stickycode.stereotype.StickyPlugin;
 
-public class SpringStickyBootstrap
+public class Spring3StickyBootstrap
     implements StickyBootstrap {
 
   private GenericApplicationContext context;
 
-  public SpringStickyBootstrap() {
+  public Spring3StickyBootstrap() {
     this.context = new GenericApplicationContext();
     scan("net.stickycode");
   }
 
-  public SpringStickyBootstrap(GenericApplicationContext context) {
+  public Spring3StickyBootstrap(GenericApplicationContext context) {
     this.context = context;
     scan("net.stickycode");
   }
 
-  public SpringStickyBootstrap(String... paths) {
+  public Spring3StickyBootstrap(String... paths) {
     this(new GenericApplicationContext());
     if (paths != null && paths.length > 0)
       scan(paths);
@@ -37,7 +38,8 @@ public class SpringStickyBootstrap
     scanner.setScopeMetadataResolver(new StickyScopeMetadataResolver());
     scanner.addIncludeFilter(new AnnotationTypeFilter(StickyComponent.class));
     scanner.addIncludeFilter(new AnnotationTypeFilter(StickyPlugin.class));
-    scanner.addIncludeFilter(new AnnotationTypeFilter(org.springframework.stereotype.Component.class));
+    scanner.addIncludeFilter(new AnnotationTypeFilter(StickyDomain.class));
+    scanner.addIncludeFilter(new AnnotationTypeFilter(Component.class));
     scanner.scan(paths);
     return this;
   }
@@ -50,8 +52,9 @@ public class SpringStickyBootstrap
   }
 
   @Override
-  public void inject(Object value) {
+  public StickyBootstrap inject(Object value) {
     getAutowirer().autowireBean(value);
+    return this;
   }
 
   @Override
