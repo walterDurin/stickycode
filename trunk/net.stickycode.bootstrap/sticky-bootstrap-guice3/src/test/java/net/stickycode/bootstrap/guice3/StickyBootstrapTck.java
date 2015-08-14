6f9da65b4ebddf6ea17da3eb6cfdea1d6e14e275
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-14 11:03:17 +0000

    doh

--- trunk/net.stickycode.bootstrap/sticky-bootstrap-guice3/src/test/java/net/stickycode/bootstrap/guice3/StickyBootstrapTck.java
+++ trunk/net.stickycode.bootstrap/sticky-bootstrap-guice3/src/test/java/net/stickycode/bootstrap/guice3/StickyBootstrapTck.java
@@ -10,13 +10,14 @@ import net.stickycode.bootstrap.tck.component.ComponentTest;
 import net.stickycode.bootstrap.tck.component.ContractComponentTest;
 import net.stickycode.bootstrap.tck.configured.ConfiguredTest;
 import net.stickycode.bootstrap.tck.plugin.PluggableTest;
+import net.stickycode.bootstrap.tck.provider.ProviderTest;
 
 @SuiteClasses({
     ComponentTest.class,
     ContractComponentTest.class,
     ConfiguredTest.class,
     PluggableTest.class,
-    Provider.class
+    ProviderTest.class
 })
 @RunWith(Suite.class)
 public class StickyBootstrapTck {
