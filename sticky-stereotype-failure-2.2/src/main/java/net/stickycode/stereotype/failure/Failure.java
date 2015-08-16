8ea94ae565d40d92e1ed2ea03fff48a7b2b8d245
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-16 09:22:12 +0000

    Releasing sticky-stereotype-failure at 2.2-SNAPSHOT …copy for tag sticky-stereotype-failure-2.2

---
+++ sticky-stereotype-failure-2.2/src/main/java/net/stickycode/stereotype/failure/Failure.java
@@ -0,0 +1,23 @@
+package net.stickycode.stereotype.failure;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface Failure {
+
+  FailureClassification value();
+
+  /**
+   * How the failure can be resolved, this is an indication for the receiver in terms of handling the error.
+   */
+  FailureResolution resolution() default FailureResolution.Application;
+
+}
---
+++ sticky-stereotype-failure-2.2/src/test/java/net/stickycode/stereotype/failure/resolver/QuotedParameterResolverTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2010 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.stereotype.failure.resolver;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+import org.junit.Test;
+
+import net.stickycode.stereotype.failure.BlankParameterFailure;
+import net.stickycode.stereotype.failure.InvalidParameterFailure;
+import net.stickycode.stereotype.failure.NullParameterFailure;
+import net.stickycode.stereotype.failure.resolver.ParameterResolver;
+
+
+public class QuotedParameterResolverTest {
+
+  @Test(expected=NullPointerException.class)
+  public void nullMessageExcepts() {
+    ParameterResolver.resolve(null);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void emptyMessageExcepts() {
+    ParameterResolver.resolve("");
+  }
+
+  @Test(expected=TooManyArgumentsException.class)
+  public void noPlaceHolderForArgumentExcepts() {
+    ParameterResolver.resolve("a message with no placeholders", "a");
+  }
+
+  @Test(expected=ThrowablesCannotBeParametersException.class)
+  public void throwableArgumentsExceptAsItsACommonMistake() {
+    ParameterResolver.resolve("placeholder and throwable argument ''", new RuntimeException());
+  }
+
+  @Test
+  public void onePlaceHolderOneArgument() {
+    assertThat(ParameterResolver.resolve("''", "a")).isEqualTo("'a'");
+  }
+
+  @Test
+  public void twoPlaceHolderTwoArgument() {
+    assertThat(ParameterResolver.resolve("''''", "a", "b")).isEqualTo("'a''b'");
+    assertThat(ParameterResolver.resolve("z''z''z", "a", "b")).isEqualTo("z'a'z'b'z");
+  }
+
+  @Test
+  public void checkThatPartialPlaceHoldersDontCauseIssues() {
+    assertThat(ParameterResolver.resolve("z' '' 'z''z", "a", "b")).isEqualTo("z' 'a' 'z'b'z");
+    assertThat(ParameterResolver.resolve("'")).isEqualTo("'");
+    assertThat(ParameterResolver.resolve("z' \\'\\' 'z''z", "a")).isEqualTo("z' \\'\\' 'z'a'z");
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void checkThatNullParametersExcept() {
+    ParameterResolver.resolve("''", (Object[])null);
+  }
+
+  @Test(expected=NullPointerException.class)
+  public void checkThatANullParameterExcepts() {
+    ParameterResolver.resolve("''", new Object[] {null});
+  }
+
+  @Test(expected=TooManyArgumentsException.class)
+  public void tooManyArguments() {
+    ParameterResolver.resolve("''", "a", "b");
+  }
+
+  @Test(expected=TooManyPlaceHoldersException.class)
+  public void tooManyPlaceHolders() {
+    ParameterResolver.resolve("'' ''", "b");
+  }
+
+}
