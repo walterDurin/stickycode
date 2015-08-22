fc45ee64cce710480aabbd96a03f5b6dc75a285c
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 09:44:59 +0000

    simplified guice usage now no need for extra scanners

--- trunk/net.stickycode.bootstrap/sticky-bootstrap-guice3/src/main/java/net/stickycode/bootstrap/guice3/Guice3StickyBootstrap.java
+++ trunk/net.stickycode.bootstrap/sticky-bootstrap-guice3/src/main/java/net/stickycode/bootstrap/guice3/Guice3StickyBootstrap.java
@@ -1,40 +1,40 @@
 package net.stickycode.bootstrap.guice3;
 
-import static net.stickycode.bootstrap.guice3.StickyModule.applicationModule;
-import static net.stickycode.bootstrap.guice3.StickyModule.bootstrapModule;
-import static net.stickycode.bootstrap.guice3.StickyModule.keyBuilderModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.ProvisionException;
 
 import net.stickycode.bootstrap.StickyBootstrap;
 
 public class Guice3StickyBootstrap
     implements StickyBootstrap {
 
+  private Logger log = LoggerFactory.getLogger(getClass());
+
   private Injector injector;
-
-  public Guice3StickyBootstrap() {
-    this.injector = Guice.createInjector(
-        bootstrapModule(),
-        keyBuilderModule());
-  }
-
-  public static Injector createInjector(Injector injector, String... packages) {
-    return injector
-        .createChildInjector(applicationModule(packages));
-  }
 
   @Override
   public StickyBootstrap scan(String... packages) {
-    this.injector = injector
-        .createChildInjector(applicationModule(packages));
+    this.injector = Guice.createInjector(
+        new StickyModule(packages));
     return this;
   }
 
   @Override
   public StickyBootstrap inject(Object instance) {
-    injector.injectMembers(instance);
+    try {
+      injector.injectMembers(instance);
+    }
+    catch (ProvisionException e) {
+      if (e.getCause() instanceof RuntimeException) {
+        log.error("Unrolling provision failure {}", e.getMessage());
+        throw (RuntimeException) e.getCause();
+      }
+      throw e;
+    }
     return this;
   }
 
@@ -42,7 +42,6 @@ public class Guice3StickyBootstrap
   public <T> T find(Class<T> type) {
     return injector.getInstance(type);
   }
-
 
 
 }
