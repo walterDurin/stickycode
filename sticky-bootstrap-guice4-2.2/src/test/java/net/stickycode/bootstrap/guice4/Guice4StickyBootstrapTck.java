65eff86acf975dc7edade249aac70d006be0702a
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 09:37:19 +0000

    Releasing sticky-bootstrap-guice4 at 2.2-SNAPSHOT …copy for tag sticky-bootstrap-guice4-2.2

---
+++ sticky-bootstrap-guice4-2.2/src/test/java/net/stickycode/bootstrap/guice4/Guice4StickyBootstrapTck.java
@@ -0,0 +1,15 @@
+package net.stickycode.bootstrap.guice4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import net.stickycode.bootstrap.tck.StickyBootstrapTck;
+
+@SuiteClasses({
+    StickyBootstrapTck.class
+})
+@RunWith(Suite.class)
+public class Guice4StickyBootstrapTck {
+
+}
