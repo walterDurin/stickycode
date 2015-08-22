8ad7b806988d5b603d288d38147a713eebee7537
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 09:37:19 +0000

    Releasing sticky-bootstrap-guice4 at 2.2-SNAPSHOT …copy for tag sticky-bootstrap-guice4-2.2

---
+++ tags/net.stickycode.bootstrap/sticky-bootstrap-guice4-2.2/src/main/java/net/stickycode/bootstrap/guice4/Guice4StickyBootstrap.java
@@ -0,0 +1,46 @@
+package net.stickycode.bootstrap.guice4;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.ProvisionException;
+
+import net.stickycode.bootstrap.StickyBootstrap;
+
+public class Guice4StickyBootstrap
+    implements StickyBootstrap {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  private Injector injector;
+
+  @Override
+  public StickyBootstrap scan(String... packages) {
+    this.injector = Guice.createInjector(
+        new StickyModule(packages));
+    return this;
+  }
+
+  @Override
+  public StickyBootstrap inject(Object instance) {
+    try {
+      injector.injectMembers(instance);
+    }
+    catch (ProvisionException e) {
+      if (e.getCause() instanceof RuntimeException) {
+        log.error("Unrolling provision failure {}", e.getMessage());
+        throw (RuntimeException) e.getCause();
+      }
+      throw e;
+    }
+    return this;
+  }
+
+  @Override
+  public <T> T find(Class<T> type) {
+    return injector.getInstance(type);
+  }
+
+}
