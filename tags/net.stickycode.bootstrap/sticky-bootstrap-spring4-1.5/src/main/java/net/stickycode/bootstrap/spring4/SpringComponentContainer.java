3c43c7a8871bfe0c5902c480f6cbaa7039bed7a9
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 11:51:04 +0000

    Releasing sticky-bootstrap-spring4 at 1.5-SNAPSHOT …copy for tag sticky-bootstrap-spring4-1.5

---
+++ tags/net.stickycode.bootstrap/sticky-bootstrap-spring4-1.5/src/main/java/net/stickycode/bootstrap/spring4/SpringComponentContainer.java
@@ -0,0 +1,45 @@
+package net.stickycode.bootstrap.spring4;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+
+import net.stickycode.bootstrap.BeanNotFoundFailure;
+import net.stickycode.bootstrap.ComponentContainer;
+import net.stickycode.stereotype.StickyComponent;
+
+@StickyComponent
+public class SpringComponentContainer
+    implements ComponentContainer {
+
+  @Inject
+  private ApplicationContext factory;
+
+  @Override
+  public void inject(Object value) {
+    factory.getAutowireCapableBeanFactory().autowireBean(value);
+  }
+
+  @Override
+  public <T> T find(Class<T> type) {
+    try {
+      return factory.getBean(type);
+    }
+    catch (NoSuchBeanDefinitionException e) {
+      try {
+        Map<String, T> beans = factory.getBeansOfType(type);
+        if (beans.values().isEmpty())
+          throw new BeanNotFoundFailure(e, type);
+
+        throw new BeanNotFoundFailure(type, beans.values());
+      }
+      catch (NoSuchBeanDefinitionException e2) {
+        throw new BeanNotFoundFailure(e2, type);
+      }
+    }
+  }
+
+}
