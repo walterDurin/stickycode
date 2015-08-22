b0d04ad7e16e86a813adfc4c235b1c54abe84d70
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 12:08:21 +0000

    Releasing sticky-configured-guice3 at 4.1-SNAPSHOT …copy for tag sticky-configured-guice3-4.1

---
+++ tags/net.stickycode.configured/sticky-configured-guice3-4.1/src/test/java/net/stickycode/configured/guice3/Guice3ConfiguredTck.java
@@ -0,0 +1,13 @@
+package net.stickycode.configured.guice3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import net.stickycode.configured.ConfiguredTck;
+
+@SuiteClasses({ ConfiguredTck.class })
+@RunWith(Suite.class)
+public class Guice3ConfiguredTck {
+
+}
