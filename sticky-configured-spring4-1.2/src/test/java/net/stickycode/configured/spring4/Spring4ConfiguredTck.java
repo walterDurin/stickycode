77ae2cfbd463169dfac7f412a5564ca0f32842f2
michael <michael@b32a16e9-343b-bee1-a1d4-014aecb29fd9>
2015-08-22 12:05:48 +0000

    Releasing sticky-configured-spring4 at 1.2-SNAPSHOT …copy for tag sticky-configured-spring4-1.2

---
+++ sticky-configured-spring4-1.2/src/test/java/net/stickycode/configured/spring4/Spring4ConfiguredTck.java
@@ -0,0 +1,13 @@
+package net.stickycode.configured.spring4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import net.stickycode.configured.ConfiguredTck;
+
+@SuiteClasses({ConfiguredTck.class})
+@RunWith(Suite.class)
+public class Spring4ConfiguredTck {
+
+}
